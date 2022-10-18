a2171fee4fb502fb5ea043ad5c209bbd61cee6b9
llandeiralabelgrup <llandeira@labelgrup.com>
2022-10-18 09:17:19 +0200

    JESUSVICO creación de estáticas (no acabadas)

---
+++ resources/views/default_v2/includes/statics/how_to_sell.blade.php
@@ -0,0 +1,143 @@
+<style>
+	.how-to-sell p {
+		font-size: 16pt;
+	}
+
+	.how-to-sell h2 {
+		margin: 1.5rem 0 1.5rem 0;
+	}
+
+	.bids-container .option-links,
+	.bids-container .option {
+		margin-left: 30px;
+	}
+
+	.bids-container section {
+		padding: 15px 0;
+	}
+
+	.bids-container .option-links {
+		display: flex;
+		align-items: center;
+		font-size: 14pt;
+		font-weight: 200;
+		color: var(--lb-color-gray);
+	}
+
+	.bids-container .option-links a:hover,
+	.bids-container .option-links a:focus,
+	.bids-container .option-links a {
+		color: var(--lb-color-gray);
+		text-decoration: none;
+	}
+
+	.bids-container h4 {
+		margin: 0;
+		margin-bottom: 3px;
+	}
+
+	.how-to-sell .flex-align-center {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+	}
+
+	.how-to-sell .lb-blockquote {
+		max-width: none;
+	}
+</style>
+
+
+<div class="how-to-sell">
+
+	<div class="row">
+		<div class="col-12 col-sm-8">
+
+			<div class="decoration">
+				<video style="width: 80%; height: auto;" controls="" poster="/themes/jesusvico/assets/img/how_to_sell_poster.jpg"
+					autoplay="">
+					<source src="/files/videos/jesusvico_tasaciones.mp4" type="video/mp4">
+					Tu navegador no soporta HTML5 video.
+				</video>
+			</div>
+
+		</div>
+		<div class="col-12 col-sm-4">
+
+			<div class="flex-align-center" style="margin-bottom: 30px">
+				<div class="mb-5">
+					<h3>Si está pensando en vender póngase en contacto con nosotros.</h3>
+					<h3>Le ofreceremos dos posibilidades para dar salida a sus piezas o a su colección.</h3>
+				</div>
+
+				{{-- Flecha --}}
+				<svg xmlns="http://www.w3.org/2000/svg" width="49.162" height="40.404" viewBox="0 0 49.162 40.404">
+					<g id="Grupo_40" data-name="Grupo 40" transform="translate(-240.156 -563.202)">
+						<path id="Trazado_2" data-name="Trazado 2" d="M140.429,44.608,164.3,68.482l23.874-23.874"
+							transform="translate(100.433 533.71)" fill="none" stroke="#b9b13c" stroke-width="2" />
+						<path id="Trazado_3" data-name="Trazado 3" d="M140.429,44.608,164.3,68.482l23.874-23.874"
+							transform="translate(100.433 519.301)" fill="none" stroke="#b9b13c" stroke-width="2" opacity="0.398" />
+					</g>
+				</svg>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12">
+			<section>
+				<h2>Subasta</h2>
+				<ul class="style-diamond">
+					<li>
+						<p>Envíenos un email, llámenos o visite nuestras oficinas para una primera toma de contacto.</p>
+						<p>Estudiaremos el material facilitado. Si es preciso, nos desplazaremos para evaluar las piezas y efectuar un
+							análisis más exhaustivo.</p>
+						<p>Esta primera tasación es <strong>gratuíta</strong> y <strong>sin compromiso</strong>.</p>
+					</li>
+					<li>
+						<p>En caso de acuerdo por ambas partes, consignaremos el material y, desde ese momento, quedará asegurado y
+							comenzaremos a trabajar con él. Le entregaremos el documento correspondiente con la relación de los lotes y sus
+							precios de salida.</p>
+					</li>
+					<li>
+						<p>Si así se ha acordado, podremos efectuar <strong>anticipos</strong> de hasta un 50% sobre el precio de salida.
+						</p>
+					</li>
+					<li>
+						<p>Una vez consignadas las piezas, no permitiremos su retirada salvo causa justificada.</p>
+					</li>
+					<li>
+						<p>No efectuaremos ningún tipo de cargo sobre los lotes no vendidos.</p>
+						<p>Sobre los lotes no vendidos, cargaremos la comisión pactada previamente.</p>
+					</li>
+					<li>
+						<p>El pago de la liquidación de los lotes vendidos se realizará a los 40 días de la celebración de la subasta.</p>
+					</li>
+				</ul>
+			</section>
+			<section>
+				<h2>Venta directa</h2>
+				<ul class="style-diamond">
+					<li>
+						<p>En Jesús Vico, le ofrecemos la posibilidad de vender sus piezas mediante un contrato de compraventa</p>
+						<br>
+						<p>Póngase en contacto con nosotros o envíenos fotografías del material que desee vender por email o en nuestro
+							apartado de <strong>tasaciones y valoraciones</strong>.</p>
+					</li>
+				</ul>
+			</section>
+			<section>
+				<blockquote class="blockquote lb-blockquote">
+					<h3>El grupo Jesús Vico, cuenta con una <br> experiencia de más de 40 años en el sector</h3>
+				</blockquote>
+			</section>
+		</div>
+	</div>
+
+
+
+
+
+
+</div>
--- resources/views/default_v2/pages/page.blade.php
+++ resources/views/default_v2/pages/page.blade.php
@@ -16,7 +16,13 @@
 
 <div class="container">
 	<div id="pagina-{{ $data['data']->id_web_page }}" class="contenido contenido-web static-page">
-		{!! $data['data']->content_web_page !!}
+		{{-- {!! $data['data']->content_web_page !!} --}}
+
+		{{-- @include('includes.statics.how_to_buy') --}}
+		{{-- @include('includes.statics.how_to_sell') --}}
+		{{-- @include('includes.statics.who_we_are') --}}
+		@include('includes.statics.laboratory')
+
 	</div>
 </div>
 @stop
