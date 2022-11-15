fc26a9cc5aafa324a302c623cb2b635d362b338a
Eloy N <enadal@labelgrup.com>
2022-11-15 17:00:45 +0100

    jesus vico v2 add animations in legal advice page

--- public/themes/jesusvico/custom.js
+++ public/themes/jesusvico/custom.js
@@ -6,7 +6,46 @@ project = {
 
 $(function () {
 	$('#js-show-filters, #js-hide-filters').on('click', resizeGridBanner);
+
+	createObservers('.observer-animation-bottom');
 })
+
+function createObservers(elementSelector) {
+
+	const targets = [...document.querySelectorAll(elementSelector)];
+	if(targets.length === 0){
+		return;
+	}
+
+	const options = {
+		rootMargin: '0px',
+		threshold: 0.8
+	}
+
+	const observer = new IntersectionObserver(handleIntersect, options);
+
+	targets.forEach(target => {
+		observer.observe(target);
+	});
+}
+
+function handleIntersect(entries, observer) {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting && entry.target.classList.contains('opacity-0')) {
+			entry.target.classList.remove('opacity-0');
+			entry.target.classList.add('lb-fadeIn')
+			entry.target.classList.add('lb-fadeInBottom');
+		}
+	});
+}
+
+function scrollToElement(element, miliseconds) {
+	const speed = miliseconds ?? 600;
+	$('html,body').animate({
+		scrollTop: $(element).offset().top - 70
+	}, speed);
+}
+
 
 carrousel_molon_new = function (carrousel) {
 	if (carrousel.data('hasSlick')) {
