5dc5a370688c7bf398c040e0495ddf4ab615b1e4
Eloy N <enadal@labelgrup.com>
2022-11-23 09:18:57 +0100

    add missing route

--- routes/web.php
+++ routes/web.php
@@ -115,6 +115,9 @@ Route::post('/subasta/modal_images_fullscreen', 'SubastaController@modalImagesFu
 #lotes
 
 Route::get(Routing::slugSeo('lote') . '/{cod}-{texto2}/{ref}-{texto}', 'SubastaController@lote')->where(array('cod' => '[0-9a-zA-Z]+', 'page' => '[0-9]+',));
+#NewLotes
+Route::get(Routing::slugSeo('subasta-lote') . '/{texto}/{cod}-{ref}', 'SubastaController@lote')->where(array('cod' => '[0-9a-zA-Z]+'));
+
 //2017-11-08  no parece que se use
 //Route::get(Routing::slugSeo('lote').'/{cod}/{ref}-{texto}', 'SubastaController@lote')->where(array('cod' => '[0-9a-zA-Z]+', 'page' => '[0-9]+',));
 #listado de lotes categorias y tematicos
