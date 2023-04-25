c7508b6807afe0a1156d2d5f2820b9276b4dc343
Eloy N <enadal@labelgrup.com>
2023-04-25 16:04:24 +0200

    jesusvico add extrainfo (desc_det) in lot desctiption

--- resources/views/themes/jesusvico/includes/ficha/ficha_description.blade.php
+++ resources/views/themes/jesusvico/includes/ficha/ficha_description.blade.php
@@ -1,45 +1,55 @@
 @php
-$category = App\Models\V5\FgOrtsec1::select('lin_ortsec1', 'des_ortsec0', 'key_ortsec0')->JoinFgOrtsec0()->where('sec_ortsec1', $lote_actual->sec_hces1)->first();
-$caracteristicas = App\Models\V5\FgCaracteristicas_Hces1::getByLot($lote_actual->num_hces1, $lote_actual->lin_hces1);
+    $category = App\Models\V5\FgOrtsec1::select('lin_ortsec1', 'des_ortsec0', 'key_ortsec0')
+        ->JoinFgOrtsec0()
+        ->where('sec_ortsec1', $lote_actual->sec_hces1)
+        ->first();
+    $caracteristicas = App\Models\V5\FgCaracteristicas_Hces1::getByLot($lote_actual->num_hces1, $lote_actual->lin_hces1);
 @endphp
 
 <div class="description">
-	<div class="max-lines mb-2 h-auto" style="--max-lines: 3; max-height: initial">
-		<h1 class="h4 no-decoration fw-light" >{{ $lote_actual->descweb_hces1 }}</h1>
-	</div>
-	<div class="long-description text-lb-gray max-lines with-scroll lb-scroll">
-		<p class="fw-lighter">
-			{!! str_replace('&nbsp;', ' ', $lote_actual->desc_hces1) !!}
-			{{-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nemo rem illo incidunt esse, eius earum possimus voluptates vitae sequi, laudantium perferendis repudiandae molestiae? Fugit voluptatibus nemo eligendi quo voluptate. --}}
-		</p>
-	</div>
+    <div class="max-lines mb-2 h-auto" style="--max-lines: 3; max-height: initial">
+        <h1 class="h4 no-decoration fw-light">{{ $lote_actual->descweb_hces1 }}</h1>
+    </div>
+    <div class="long-description text-lb-gray max-lines with-scroll lb-scroll">
+        <p class="fw-lighter">
+            {!! str_replace('&nbsp;', ' ', $lote_actual->desc_hces1) !!}
+        </p>
+    </div>
 
+    @if (!empty($lote_actual->descdet_hces1))
+        <p class="provenance">
+            {{ $lote_actual->descdet_hces1 }}
+        </p>
+    @endif
 </div>
 
-@if(count($caracteristicas) !== 0)
-	<div class="features mt-3">
-		<h5>{{ trans("$theme-app.features.features") }}</h5>
 
-		<table class="w-100">
-			<tbody>
-				@foreach($caracteristicas as $caracteristica)
-				<tr>
-					<td>{{$caracteristica->name_caracteristicas}}</td>
-					<td>{{$caracteristica->value_caracteristicas_hces1}}</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
 
-	</div>
+@if (count($caracteristicas) !== 0)
+    <div class="features mt-3">
+        <h5>{{ trans("$theme-app.features.features") }}</h5>
+
+        <table class="w-100">
+            <tbody>
+                @foreach ($caracteristicas as $caracteristica)
+                    <tr>
+                        <td>{{ $caracteristica->name_caracteristicas }}</td>
+                        <td>{{ $caracteristica->value_caracteristicas_hces1 }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
 @endif
 
-@if(!empty($category))
-<div class="categories">
-	<h5>{{ trans(\Config::get('app.theme').'-app.lot.categories') }}</h5>
+@if (!empty($category))
+    <div class="categories">
+        <h5>{{ trans(\Config::get('app.theme') . '-app.lot.categories') }}</h5>
 
-	<a class="no-decoration" href="{{ route("category", ["keycategory" => $category->key_ortsec0]) }}" alt="{{$category->des_ortsec0}}">
-		<span class="badge badge-custom-primary">{{$category->des_ortsec0}}</span>
-	</a>
-</div>
+        <a class="no-decoration" href="{{ route('category', ['keycategory' => $category->key_ortsec0]) }}"
+            alt="{{ $category->des_ortsec0 }}">
+            <span class="badge badge-custom-primary">{{ $category->des_ortsec0 }}</span>
+        </a>
+    </div>
 @endif
