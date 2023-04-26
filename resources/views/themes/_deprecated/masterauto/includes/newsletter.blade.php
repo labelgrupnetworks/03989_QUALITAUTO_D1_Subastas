0c9e95bd8daa30f2c7a4a7e549ccd4934554ad4d
Eloy N <enadal@labelgrup.com>
2023-04-26 13:19:12 +0200

    default remove theme masterauto

--- resources/views/themes/_deprecated/masterauto/includes/newsletter.blade.php
+++
@@ -1,3 +0,0 @@
-<br><br>
-{!! \BannerLib::bannersPorKey('footer_banner', 'home-foot-banner', "{dots:false,autoplay: true, autoplaySpeed: 5000,
-slidesToScroll:1}") !!}
--- resources/views/themes/_deprecated/masterauto/pages/subastas.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.default')
-
-@section('title')
-	{{ trans(\Config::get('app.theme').'-app.head.title_app') }}
-@stop
-
-@section('content')
-	<script>window.location = "{{ \Tools::url_auction($data['auction_list'][0]->cod_sub,$data['auction_list'][0]->name,$data['auction_list'][0]->id_auc_sessions) }}";</script>
-@stop
