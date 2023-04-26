05318193b3e149976e2c69cd7e4a5b739ee3538a
Eloy N <enadal@labelgrup.com>
2023-04-26 13:23:53 +0200

    default remove theme segre_old

--- resources/views/themes/segre_old/content/tr/tiempo_real_user/streaming.blade.php
+++
@@ -1,4 +0,0 @@
-<p class="delay">{{ trans(\Config::get('app.theme').'-app.sheet_tr.delay') }}</p>
-<div class="streaming">
-    <iframe src="//stream01.labelgrup.com:5443/Stream_Segre/play.html?name=655837799519001190070454" height="288" width="512" frameborder="0" allowfullscreen></iframe>
-</div>
--- resources/views/themes/segre_old/includes/tr/tiempo_real_user/clock.blade.php
+++
@@ -1,7 +0,0 @@
-<div id="clock" style="display: block" class="clock-page">
-    <div style="margin-top: 50px">
-        <img style="display: block;max-width: 300px; margin: 0 auto" src="/themes/{{\Config::get('app.theme')}}/assets/img/logo.png"  alt="{{(\Config::get( 'app.name' ))}}">
-    </div>
-	<div data-countdown="{{strtotime($tiempo) - getdate()[0] }}"  data-format="%D {{trans(\Config::get('app.theme')."-app.msg_neutral.days")}} <br> %H:%M:%S  {{trans(\Config::get('app.theme')."-app.msg_neutral.hours")}}" data-txtend ="{{trans(\Config::get('app.theme')."-app.msg_neutral.auction_coming_soon")}}" class="tiempo wait-time text-center"></div>
-	<p class="clock-text">{!! trans(\Config::get('app.theme')."-app.sheet_tr.text_clock") !!}</p>
-</div>
