1759cff344806e5fe13d30966499c9c7adbaa7e4
Eloy N <enadal@labelgrup.com>
2023-04-26 13:31:42 +0200

    default remove theme jesusvico_old

--- resources/views/themes/jesusvico_old/content/tr/tiempo_real_user/streaming.blade.php
+++
@@ -1,4 +0,0 @@
-<p class="delay">{{ trans(\Config::get('app.theme').'-app.sheet_tr.delay') }}</p>
-<div class="streaming">
-    <iframe src="//stream01.labelgrup.com:5443/Stream_Vico/play.html?name=295504878408511467805161" height="288" width="512" frameborder="0" allowfullscreen></iframe>
-</div>
--- resources/views/themes/jesusvico_old/includes/breadcrumb_before_after.blade.php
+++
@@ -1,7 +0,0 @@
-		<div class="col-xs-12 no-padding" >
-			<div class="col-xs-12 col-sm-12 no-padding bread-after">
-				<div class="bread">
-					@include('includes.custom_bread')
-				</div>
-			</div>
-		</div>
