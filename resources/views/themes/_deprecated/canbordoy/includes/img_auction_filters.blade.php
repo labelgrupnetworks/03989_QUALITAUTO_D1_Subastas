ab70dc7977cca828ac7c34c9788b0d287af766f5
Eloy N <enadal@labelgrup.com>
2023-04-26 13:17:24 +0200

    default remove theme canbordoy

--- resources/views/themes/_deprecated/canbordoy/includes/img_auction_filters.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="sidebar_lot info-buy-filters">
-   
-    <img  width="100%" src="{{\Tools::url_img_auction('subasta_large',$ficha_subasta->cod_sub)}}" class="img-responsive">
-    <p><?= !empty($ficha_subasta->descdet_sub)? $ficha_subasta->descdet_sub : ''; ?></p>
- </div>
- 
