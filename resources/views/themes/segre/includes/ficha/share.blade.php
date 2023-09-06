91df9a13c5c38269b974b777b1d053c886cfb497
Eloy N <enadal@labelgrup.com>
2023-09-06 17:01:52 +0200

    segre change twitter icon

--- resources/views/themes/segre/includes/ficha/share.blade.php
+++ resources/views/themes/segre/includes/ficha/share.blade.php
@@ -11,7 +11,9 @@ $titulo = $titulo ?? $data['subasta_info']->lote_actual->titulo_hces1;
             <a class=" d-flex align-items-center justify-content-center color-letter" title="{{ trans(\Config::get('app.theme').'-app.lot.share_facebook') }}" href="http://www.facebook.com/sharer.php?u=<?=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ?>"><i class="fab fa-facebook-f"></i></a>
         </li>
         <li>
-            <a class=" d-flex align-items-center justify-content-center color-letter" title="{{ trans(\Config::get('app.theme').'-app.lot.share_twitter') }}" href="http://twitter.com/share?text=<?= $titulo.' '.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>&url=<?=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ?>"><i class="fab fa-twitter"></i></a>
+            <a class=" d-flex align-items-center justify-content-center color-letter" title="{{ trans(\Config::get('app.theme').'-app.lot.share_twitter') }}" href="http://twitter.com/share?text=<?= $titulo.' '.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>&url=<?=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ?>">
+				@include('components.x-icon', ['size' => '14'])
+			</a>
         </li>
         <li>
             <a class=" d-flex align-items-center justify-content-center color-letter" title="{{ trans(\Config::get('app.theme').'-app.lot.share_instagram') }}" href="https://www.instagram.com/subastassegre/"><i class="fab fa-instagram"></i></a>
