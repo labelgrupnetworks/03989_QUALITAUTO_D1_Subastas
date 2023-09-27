e83b49fa16e8fc83853f5e4b6a3f7ffe98227eaf
Eloy N <enadal@labelgrup.com>
2023-09-27 15:30:52 +0200

    segre add feature show impsahces in ficha cerrada

--- resources/views/themes/segre/includes/ficha/pujas_ficha_cerrada.blade.php
+++ resources/views/themes/segre/includes/ficha/pujas_ficha_cerrada.blade.php
@@ -21,7 +21,9 @@ else{
 }
 
 //Si hay precio de venta y impsalweb_asigl0 contiene valor, mostramos este como precio de venta
-$precio_venta = (!empty($precio_venta) && $lote_actual->impsalweb_asigl0 != 0) ? $lote_actual->impsalweb_asigl0 : $precio_venta;
+//$precio_venta = (!empty($precio_venta) && $lote_actual->impsalweb_asigl0 != 0) ? $lote_actual->impsalweb_asigl0 : $precio_venta;
+
+//dd($lote_actual);
 ?>
 <script src="https://www.google.com/recaptcha/api.js?hl={{ \Config::get('app.locale') }}" async defer></script>
 <div class="info_single lot-sold col-xs-12 no-padding">
@@ -48,6 +50,13 @@ $precio_venta = (!empty($precio_venta) && $lote_actual->impsalweb_asigl0 != 0) ?
                     </div>
 				@else {{--if(!$sub_historica && !$sub_cerrada ) --}}
 					{{-- Formulario de  petición de información--}}
+					@if(!empty($lote_actual->impsalhces_asigl0))
+					<div class="pre lot-sold_impsal">
+                        <p class="pre-title-principal adj-text">{{ trans(\Config::get('app.theme').'-app.lot.lot-price') }}</p>
+                        <p class="pre-price">{{ \Tools::moneyFormat($lote_actual->impsalhces_asigl0) }} {{ trans(\Config::get('app.theme').'-app.subastas.euros') }}</p>
+                    </div>
+					@endif
+
 					<p class="pre-title-principal adj-text">	{{ trans(\Config::get('app.theme').'-app.galery.request_information') }} </p>
 					<form name="infoLotForm" id="infoLotForm" method="post" action="javascript:sendInfoLot()">
 						<input type="hidden" name="auction" value="{{ $lote_actual->cod_sub}} - {{ $lote_actual->des_sub}}">
