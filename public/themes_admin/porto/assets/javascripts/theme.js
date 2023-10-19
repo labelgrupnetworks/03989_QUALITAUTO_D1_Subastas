94be35febbfbcd12d68e26c0953debf82b86f1af
rsanchez <rsanchez@labelgrup.com>
2023-10-19 10:03:33 +0200

    textos de tinymce que no se podía escribir

--- public/themes_admin/porto/assets/javascripts/theme.js
+++ public/themes_admin/porto/assets/javascripts/theme.js
@@ -4499,3 +4499,11 @@ window.theme = {};
 	}
 
 }).apply(this, [jQuery]);
+
+// Prevent Bootstrap dialog from blocking focusin
+$(document).on('focusin', function(e) {
+
+	if ($(e.target).closest(".tox-tinymce, .tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
+	  e.stopImmediatePropagation();
+	}
+  });
