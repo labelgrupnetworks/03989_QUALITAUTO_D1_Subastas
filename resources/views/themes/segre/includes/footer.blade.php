a45178ae8336465d791bdf7fddc9b63990f2c4af
Eloy N <enadal@labelgrup.com>
2023-10-30 16:26:01 +0100

    segre add modal cookies feature:cookies

--- resources/views/themes/segre/includes/footer.blade.php
+++ resources/views/themes/segre/includes/footer.blade.php
@@ -1,141 +1,136 @@
-<?php
-$empre = new \App\Models\Enterprise();
-$empresa = $empre->getEmpre();
-?>
-
 <footer>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-xs-12 col-md-4">
-				<h4 class="footer-title">{{ trans($theme.'-app.foot.contact_title') }}</h4>
-				<div class="footer-content">
-					{!! trans($theme.'-app.foot.contact_info') !!}
-					<a class="footer-link" href="{{ route('catalogos_newsletter') }}">{{ trans($theme . '-app.foot.receive_catalog') }}</a>
-				</div>
-				<div class="social-link-container">
-					@if (\Config::get('app.facebook'))
-						<a class="facebook-social-link social-link" href="{{ \Config::get('app.facebook') }}" target="_blank">
-							<i class="fa fa-facebook-square social-link-icon" aria-hidden="true"></i>
-						</a>
-					@endif
-					@if (\Config::get('app.twitter'))
-						<a class="twitter-social-link social-link" href="{{ \Config::get('app.twitter') }}" target="_blank">
-							@include('components.x-icon', ['size' => '40'])
-						</a>
-					@endif
-					@if (\Config::get('app.youtube'))
-						<a class="youtube-social-link social-link" href="{{ \Config::get('app.youtube') }}" target="_blank">
-							<i class="fa fa-youtube-square social-link-icon" aria-hidden="true"></i>
-						</a>
-					@endif
-					@if (\Config::get('app.instagram'))
-						<a class="instagram-social-link social-link" href="{{ \Config::get('app.instagram') }}" target="_blank">
-							<i class="fa fa-instagram social-link-icon" aria-hidden="true"></i>
-						</a>
-					@endif
-					@if (\Config::get('app.linkedin'))
-						<a class="linkedin-social-link social-link" href="{{ \Config::get('app.linkedin') }}" target="_blank">
-							<i class="fa fa-linkedin-square social-link-icon" aria-hidden="true"></i>
-						</a>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-md-4">
+                <h4 class="footer-title">{{ trans($theme . '-app.foot.contact_title') }}</h4>
+                <div class="footer-content">
+                    {!! trans($theme . '-app.foot.contact_info') !!}
+                    <a class="footer-link"
+                        href="{{ route('catalogos_newsletter') }}">{{ trans($theme . '-app.foot.receive_catalog') }}</a>
+                </div>
+                <div class="social-link-container">
+                    @if (\Config::get('app.facebook'))
+                        <a class="facebook-social-link social-link" href="{{ \Config::get('app.facebook') }}"
+                            target="_blank">
+                            <i class="fa fa-facebook-square social-link-icon" aria-hidden="true"></i>
+                        </a>
+                    @endif
+                    @if (\Config::get('app.twitter'))
+                        <a class="twitter-social-link social-link" href="{{ \Config::get('app.twitter') }}"
+                            target="_blank">
+                            @include('components.x-icon', ['size' => '40'])
+                        </a>
+                    @endif
+                    @if (\Config::get('app.youtube'))
+                        <a class="youtube-social-link social-link" href="{{ \Config::get('app.youtube') }}"
+                            target="_blank">
+                            <i class="fa fa-youtube-square social-link-icon" aria-hidden="true"></i>
+                        </a>
+                    @endif
+                    @if (\Config::get('app.instagram'))
+                        <a class="instagram-social-link social-link" href="{{ \Config::get('app.instagram') }}"
+                            target="_blank">
+                            <i class="fa fa-instagram social-link-icon" aria-hidden="true"></i>
+                        </a>
+                    @endif
+                    @if (\Config::get('app.linkedin'))
+                        <a class="linkedin-social-link social-link" href="{{ \Config::get('app.linkedin') }}"
+                            target="_blank">
+                            <i class="fa fa-linkedin-square social-link-icon" aria-hidden="true"></i>
+                        </a>
+                    @endif
+                </div>
+            </div>
 
-					@endif
-				</div>
-			</div>
+            <div class="col-xs-12 col-md-4">
+                <section>
+                    <h4 class="footer-title">{{ trans($theme . '-app.foot.shcedule_title') }}</h4>
+                    <div class="footer-content">
+                        <p>
+                            {!! trans($theme . '-app.foot.segre_schedule') !!}
+                        </p>
+                    </div>
+                </section>
+                <section>
+                    <h4 class="footer-title">{{ trans($theme . '-app.foot.download_app') }}</h4>
+                    <div class="footer-content">
+                        <a href="{{ trans($theme . '-app.segre-enlaces.segre_app_apple') }}" target="_blank">
+                            <img class=""
+                                src="/themes/{{ \Config::get('app.theme') }}/assets/img/1logoapple200.png"
+                                alt="" width="200" height="72">
+                        </a>
 
-			<div class="col-xs-12 col-md-4">
-				<section>
-					<h4 class="footer-title">{{ trans($theme.'-app.foot.shcedule_title') }}</h4>
-					<div class="footer-content">
-						<p>
-							{!! trans($theme.'-app.foot.segre_schedule') !!}
-						</p>
-					</div>
-				</section>
-				<section>
-					<h4 class="footer-title">{{ trans($theme.'-app.foot.download_app') }}</h4>
-					<div class="footer-content">
-						<a href="{{ trans($theme.'-app.segre-enlaces.segre_app_apple') }}" target="_blank">
-							<img class="" src="/themes/{{ \Config::get('app.theme') }}/assets/img/1logoapple200.png" alt=""
-								width="200" height="72">
-						</a>
+                        <a href="{{ trans($theme . '-app.segre-enlaces.segre_app_android') }}" target="_blank">
+                            <img class=""
+                                src="/themes/{{ \Config::get('app.theme') }}/assets/img/1logoandroid200.png"
+                                alt="" width="200" height="72">
+                        </a>
+                    </div>
+                </section>
+            </div>
 
-						<a href="{{ trans($theme.'-app.segre-enlaces.segre_app_android') }}" target="_blank">
-							<img class="" src="/themes/{{ \Config::get('app.theme') }}/assets/img/1logoandroid200.png" alt=""
-								width="200" height="72">
-						</a>
-					</div>
-				</section>
-			</div>
-
-			<div class="col-xs-12 col-md-4">
-				<h4 class="footer-title">{{ trans($theme.'-app.foot.legal') }}</h4>
-				<div class="footer-content">
-					<ul>
+            <div class="col-xs-12 col-md-4">
+                <h4 class="footer-title">{{ trans($theme . '-app.foot.legal') }}</h4>
+                <div class="footer-content">
+                    <ul>
+                        <li class="footer-list">
+                            <a class="footer-link"
+                                href="{{ Routing::translateSeo('pagina') . trans($theme . '-app.links.general-conditions') }}">
+                                {{ trans($theme . '-app.foot.general-conditions') }}
+                            </a>
+                        </li>
+                        <li class="footer-list">
+                            <a class="footer-link"
+                                href="{{ Routing::translateSeo('pagina') . trans($theme . '-app.links.legal-warning') }}">
+                                {{ trans($theme . '-app.foot.legal-warning') }}
+                            </a>
+                        </li>
+                        <li class="footer-list">
+                            <a class="footer-link"
+                                href="{{ Routing::translateSeo('pagina') . trans($theme . '-app.links.privacy_policy') }}">
+                                {{ trans($theme . '-app.foot.privacy_policy') }}
+                            </a>
+                        </li>
+                        <li class="footer-list">
+                            <a class="footer-link"
+                                href="{{ Routing::translateSeo('pagina') . trans($theme . '-app.links.cookies_policy') }}">
+                                {{ trans($theme . '-app.foot.cookies_policy') }}
+                            </a>
+                        </li>
+                        <li class="footer-list">
+                            <a class="footer-link" href="{{ trans($theme . '-app.links.ethical_code') }}"
+                                target="_blank">
+                                {{ trans($theme . '-app.foot.ethical_code') }}
+                            </a>
+                        </li>
+                        <li class="footer-list">
+                            <a class="footer-link" href="{{ trans($theme . '-app.links.anticorruption_policy') }}"
+                                target="_blank">
+                                {{ trans($theme . '-app.foot.anticorruption_policy') }}
+                            </a>
+                        </li>
+                        <li class="footer-list">
+                            <a class="footer-link"
+                                href="{{ Routing::translateSeo('pagina') . trans($theme . '-app.links.shipping_returns') }}">
+                                {{ trans($theme . '-app.foot.shipping_returns') }}
+                            </a>
+                        </li>
+                        <li class="footer-list">
+                            <a class="footer-link"
+                                href="{{ Routing::translateSeo('pagina') . trans($theme . '-app.links.jewelery_watch_cataloging') }}">
+                                {{ trans($theme . '-app.foot.jewelery_watch_cataloging') }}
+                            </a>
+                        </li>
 						<li class="footer-list">
-							<a href="{{ Routing::translateSeo('pagina').trans($theme.'-app.links.general-conditions') }}" class="footer-link">
-								{{ trans($theme.'-app.foot.general-conditions') }}
-							</a>
+							<button class="footer-link footer-link-button" type="button" data-toggle="modal" data-target="#cookiesPersonalize">
+								{{ trans("$theme-app.cookies.configure") }}
+							</button>
 						</li>
-						<li class="footer-list">
-							<a href="{{ Routing::translateSeo('pagina').trans($theme.'-app.links.legal-warning') }}" class="footer-link">
-								{{ trans($theme.'-app.foot.legal-warning') }}
-							</a>
-						</li>
-						<li class="footer-list">
-							<a href="{{ Routing::translateSeo('pagina').trans($theme.'-app.links.privacy_policy') }}" class="footer-link">
-								{{ trans($theme.'-app.foot.privacy_policy') }}
-							</a>
-						</li>
-						<li class="footer-list">
-							<a href="{{ Routing::translateSeo('pagina').trans($theme.'-app.links.cookies_policy') }}" class="footer-link">
-								{{ trans($theme.'-app.foot.cookies_policy') }}
-							</a>
-						</li>
-						<li class="footer-list">
-							<a href="{{ trans($theme.'-app.links.ethical_code') }}" target="_blank" class="footer-link">
-								{{ trans($theme.'-app.foot.ethical_code') }}
-							</a>
-						</li>
-						<li class="footer-list">
-							<a href="{{ trans($theme.'-app.links.anticorruption_policy') }}" target="_blank" class="footer-link">
-								{{ trans($theme.'-app.foot.anticorruption_policy') }}
-							</a>
-						</li>
-						<li class="footer-list">
-							<a href="{{ Routing::translateSeo('pagina').trans($theme.'-app.links.shipping_returns') }}" class="footer-link">
-								{{ trans($theme.'-app.foot.shipping_returns') }}
-							</a>
-						</li>
-						<li class="footer-list">
-							<a href="{{ Routing::translateSeo('pagina').trans($theme.'-app.links.jewelery_watch_cataloging') }}" class="footer-link">
-								{{ trans($theme.'-app.foot.jewelery_watch_cataloging') }}
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-	{{-- <div class="row">
-		<div class="col-xs-12 col-sm-5 image">
-			<img class="logo-company" src="/themes/{{\Config::get('app.theme')}}/assets/img/logo_footer.png"  alt="{{(\Config::get( 'app.name' ))}}" width="90%">
-		</div>
-		<div class="col-xs-12 col-sm-7 enterprise text-justify">
-			<div class="row">
-				<div class="col-xs-12 col-sm-6 no-padding">
-					<b>< ?= !empty($empresa->nom_emp)? $empresa->nom_emp : ''; ?></b> <br>
-					< ?= !empty($empresa->dir_emp)? $empresa->dir_emp : ''; ?><br>
-					< ?= !empty($empresa->cp_emp)? $empresa->cp_emp : ''; ?> < ?= !empty($empresa->pob_emp)? $empresa->pob_emp : ''; ?> , < ?= !empty($empresa->pais_emp)? $empresa->pais_emp : ''; ?></br>
-				</div>
-				<div class="col-xs-12 col-sm-6">
-					<br>< ?= !empty($empresa->tel1_emp)? $empresa->tel1_emp : ''; ?><br>
-					<a title="< ?= !empty($empresa->email_emp)? $empresa->email_emp : ''; ?>" href="mailto:< ?= !empty($empresa->email_emp)? $empresa->email_emp : ''; ?>">
-						< ?= !empty($empresa->email_emp)? $empresa->email_emp : ''; ?>
-					</a>
-				</div>
-			</div>
-		</div>
-	</div> --}}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 
 
@@ -146,12 +141,12 @@ $empresa = $empre->getEmpre();
 
 
 <div class="copy color-letter">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-6">
-				<p>&copy; <?= trans(\Config::get('app.theme') . '-app.foot.rights') ?> </p>
-			</div>
-			{{-- <div class="col-xs-12 col-sm-6 social-links">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <p>&copy; <?= trans(\Config::get('app.theme') . '-app.foot.rights') ?> </p>
+            </div>
+            {{-- <div class="col-xs-12 col-sm-6 social-links">
 				<span class="social-links-title">< ?= trans(\Config::get('app.theme') . '-app.foot.follow_us') ?></span>
 
 				<a class="social-link color-letter"><i class="fab fa-2x fa-facebook-square"></i></a>
@@ -162,64 +157,60 @@ $empresa = $empre->getEmpre();
 				<br>
 			</div> --}}
 
-			<div class="col-xs-12 col-sm-6 text-right">
-				<a class="color-letter" role="button"
-					title="{{ trans(\Config::get('app.theme') . '-app.foot.developedSoftware') }}"
-					href="{{ trans(\Config::get('app.theme') . '-app.foot.developed_url') }}"
-					target="no_blank">{{ trans(\Config::get('app.theme') . '-app.foot.developedBy') }}</a>
-			</div>
-		</div>
-	</div>
+            <div class="col-xs-12 col-sm-6 text-right">
+                <a class="color-letter" href="{{ trans(\Config::get('app.theme') . '-app.foot.developed_url') }}"
+                    title="{{ trans(\Config::get('app.theme') . '-app.foot.developedSoftware') }}" role="button"
+                    target="no_blank">{{ trans(\Config::get('app.theme') . '-app.foot.developedBy') }}</a>
+            </div>
+        </div>
+    </div>
 </div>
 
-@if (!Cookie::get('cookie_config'))
-	@include('includes.cookie')
-	<script>
-	 cookie_law();
-	</script>
+@if (request('recoveryPassword'))
+    <script>
+        $(document).ready(function() {
+
+            var loader = $('.loader.copyme').clone().removeClass('hidden');
+
+            $('#modalAjax .modal-content .modal-title').html("¿Ha olvidado su contraseña?");
+
+
+            $.ajax({
+                url: "/es/password_recovery",
+                dataType: "text",
+
+                beforeSend: function(xhr) {
+                    $('#modalAjax .modal-content .modal-body').html(loader);
+                },
+
+                success: function(data) {
+
+                    try {
+
+                        info = $.parseJSON(data);
+
+                        if (info.status == 'error') {
+                            window.location.href = '/';
+                        }
+
+                        //$('#modalAjax .modal-content .modal-body').html("<div style='padding:20px;'>"+info.msg+"</div>");
+
+                    } catch (e) {
+                        // not json
+                        $('#modalAjax .modal-content .modal-body').html("<div style='padding:20px;'>" +
+                            data + "</div>");
+                    }
+
+                    $('#modalAjax .modal-content .loader.copyme').remove();
+                    $('#modalAjax').modal("show");
+                }
+            });
+        });
+    </script>
 @endif
 
+@if (!Cookie::get((new App\Models\Cookies())->getCookieName()))
+    @include('includes.cookie', ['style' => 'popover'])
+@endif
 
-<script>
-	@if(request("recoveryPassword"))
-
-	$(document).ready(function () {
-
-		var loader = $('.loader.copyme').clone().removeClass('hidden');
-
-		$('#modalAjax .modal-content .modal-title').html("¿Ha olvidado su contraseña?");
-
-
-		$.ajax({
-			url: "/es/password_recovery",
-			dataType: "text",
-
-			beforeSend: function (xhr) {
-				$('#modalAjax .modal-content .modal-body').html(loader);
-			},
-
-			success: function (data) {
-
-				try {
-
-					info = $.parseJSON(data);
-
-					if (info.status == 'error') {
-						window.location.href = '/';
-					}
-
-					//$('#modalAjax .modal-content .modal-body').html("<div style='padding:20px;'>"+info.msg+"</div>");
-
-				} catch (e) {
-					// not json
-					$('#modalAjax .modal-content .modal-body').html("<div style='padding:20px;'>" + data + "</div>");
-				}
-
-				$('#modalAjax .modal-content .loader.copyme').remove();
-				$('#modalAjax').modal("show");
-			}
-		});
-	});
-	@endif
-
-	</script>
+@include('includes.cookies_personalize')
