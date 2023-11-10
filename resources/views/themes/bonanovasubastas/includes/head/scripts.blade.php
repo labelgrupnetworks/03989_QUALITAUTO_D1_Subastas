953d8000dee2587c03706cba7d9c8c9b574b471c
Eloy N <enadal@labelgrup.com>
2023-11-10 13:56:15 +0100

    bonanova refactor script loading for improved performance

--- resources/views/themes/bonanovasubastas/includes/head/scripts.blade.php
+++ resources/views/themes/bonanovasubastas/includes/head/scripts.blade.php
@@ -28,17 +28,17 @@
 
 <script src="{{ URL::asset('vendor/jquery/3.6.0/dist/jquery.min.js') }}"></script>
 <script src="{{ Tools::urlAssetsCache('/js/common.js') }}"></script>
-<script src="{{ Tools::urlAssetsCache('/js/actions.js') }}"></script>
-<script src="{{ URL::asset('vendor/tiempo-real/popup/magnific-popup.js') }}"></script>
-<script src="{{ URL::asset('vendor/tiempo-real/pnotify/pnotify.custom.js') }}"></script>
-<script src="{{ URL::asset('vendor/tiempo-real/pnotify/modernizr.js') }}"></script>
-<script src="{{ URL::asset('vendor/jquery-countdown/jquery.countdown.min.js') }}"></script>
-<script src="{{ URL::asset('js/validator.js') }}"></script>
-<script src="{{ URL::asset('vendor/bootstrap/3.4.1/dist/js/bootstrap.min.js') }}"></script>
+<script defer src="{{ Tools::urlAssetsCache('/js/actions.js') }}"></script>
+<script defer src="{{ URL::asset('vendor/tiempo-real/popup/magnific-popup.js') }}"></script>
+<script defer src="{{ URL::asset('vendor/tiempo-real/pnotify/pnotify.custom.js') }}"></script>
+<script defer src="{{ URL::asset('vendor/tiempo-real/pnotify/modernizr.js') }}"></script>
+<script defer src="{{ URL::asset('vendor/jquery-countdown/jquery.countdown.min.js') }}"></script>
+<script defer src="{{ URL::asset('js/validator.js') }}"></script>
+<script defer src="{{ URL::asset('vendor/bootstrap/3.4.1/dist/js/bootstrap.min.js') }}"></script>
 
 @stack('scripts')
 
-<script src="{{ Tools::urlAssetsCache('/themes/' . $theme . '/customized_tr_main.js') }}"></script>
-<script src="{{ Tools::urlAssetsCache('/themes/' . $theme . '/custom.js') }}"></script>
-<script src="{{ URL::asset('js/forms.js') }}"></script>
+<script defer src="{{ Tools::urlAssetsCache('/themes/' . $theme . '/customized_tr_main.js') }}"></script>
+<script defer src="{{ Tools::urlAssetsCache('/themes/' . $theme . '/custom.js') }}"></script>
+<script defer src="{{ URL::asset('js/forms.js') }}"></script>
 <script src="{{ URL::asset('vendor/slick/slick.min.js') }}"></script>
