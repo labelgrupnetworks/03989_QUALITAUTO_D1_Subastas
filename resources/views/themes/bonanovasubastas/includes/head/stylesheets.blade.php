be8986a0c3cb9cda771a35afcc5d3cee3ab9a079
Eloy N <enadal@labelgrup.com>
2023-11-13 10:18:19 +0100

    bonanova preload stylesheets

--- resources/views/themes/bonanovasubastas/includes/head/stylesheets.blade.php
+++ resources/views/themes/bonanovasubastas/includes/head/stylesheets.blade.php
@@ -1,14 +1,14 @@
 <link rel="stylesheet" type="text/css" href="{{ URL::asset('vendor/bootstrap/3.4.1/dist/css/bootstrap.min.css') }}" >
 <link rel="stylesheet" href="{{ Tools::urlAssetsCache('vendor/font-awesome/4.7.0/css/font-awesome.min.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('vendor/tiempo-real/pnotify/pnotify.custom.css') }}" />
 
 <!-- Slick / banner -->
 <link rel="stylesheet" href="{{ URL::asset('vendor/slick/slick.css') }}" />
 <link rel="stylesheet" href="{{ URL::asset('vendor/slick/slick-theme.css') }}" />
 
-<!-- Magnific popup -->
-<link rel="stylesheet" href="{{ URL::asset('vendor/tiempo-real/popup/magnific-popup.css') }}" />
-<link rel="stylesheet" href="{{ Tools::urlAssetsCache('vendor/animate/3.5.2/animate.min.css') }}">
+<!-- Popups -->
+{!! Tools::preloadStylesheets("/vendor/tiempo-real/pnotify/pnotify.custom.css", false) !!}
+{!! Tools::preloadStylesheets("/vendor/tiempo-real/popup/magnific-popup.css", false) !!}
+{!! Tools::preloadStylesheets("/vendor/animate/3.5.2/animate.min.css", false) !!}
 
 @stack('styles')
 
