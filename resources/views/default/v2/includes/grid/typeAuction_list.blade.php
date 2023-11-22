888f4169b36b1705a9d7f80a154f67e2d7053567
Eloy N <enadal@labelgrup.com>
2023-11-22 17:28:19 +0100

    cmoriones change designs

--- resources/views/default/v2/includes/grid/typeAuction_list.blade.php
+++ resources/views/default/v2/includes/grid/typeAuction_list.blade.php
@@ -1,48 +1,43 @@
 <div class="auction__filters-type">
 
-	<div class="auction__filters-collapse filter-parent-collapse d-flex align-items-center justify-content-between" role="button"
-		data-bs-toggle="collapse" href="#auction_type" aria-expanded="false" aria-controls="auction_type">
+    <div class="auction__filters-collapse filter-parent-collapse d-flex align-items-center justify-content-between"
+        data-bs-toggle="collapse" href="#auction_type" role="button" aria-expanded="false" aria-controls="auction_type">
 
-		<div class="filter-title">{{ trans(\Config::get('app.theme').'-app.lot_list.auction_type') }}</div>
+        <div class="filter-title">{{ trans(\Config::get('app.theme') . '-app.lot_list.auction_type') }}</div>
 
-		<svg class="bi" width="16" height="16" fill="currentColor">
-			<use xlink:href="/bootstrap-icons.svg#caret-down-fill" />
-		</svg>
-	</div>
+        <svg class="bi" width="16" height="16" fill="currentColor">
+            <use xlink:href="/bootstrap-icons.svg#caret-down-fill" />
+        </svg>
+    </div>
 
-	<div class="input-category d-flex align-items-center d-none">
-		<input type="radio" name="typeSub" id="all_typesSub" value="" @checked(empty(request('typeSub')))/>
-	</div>
+    <div class="input-category d-flex align-items-center d-none">
+        <input id="all_typesSub" name="typeSub" type="radio" value="" @checked(empty(request('typeSub'))) />
+    </div>
 
-	<div class="auction__filters-type-list collapse filter-child-collapse mt-1" id="auction_type">
+    <div class="auction__filters-type-list collapse filter-child-collapse mt-1" id="auction_type">
 
-		@foreach($tipos_sub as $typeSub =>$desType)
-		<?php
-			$numLots = Tools::showNumLots($numActiveFilters, $filters, "typeSub", $typeSub);
-		?>
+        @foreach ($tipos_sub as $typeSub => $desType)
+            @php
+                $numLots = Tools::showNumLots($numActiveFilters, $filters, 'typeSub', $typeSub);
+            @endphp
 
-		<div class="input-type-auction d-flex align-items-center">
-			<div class="radio">
-				<?php //si no ha lotes no podrán marcar la opcion ?>
+            <div @class(['input-type-auction', 'input-type-auction--disabled' => $numLots == 0])>
+                <div class="radio">
+                    <?php //si no ha lotes no podrán marcar la opcion
+                    ?>
 
-				<input type="radio" name="typeSub" id="typeSub_{{$typeSub}}" value="{{$typeSub}}" class="filter_lot_list_js"
-					@checked($typeSub == $filters['typeSub'])
-					@disabled($numLots == 0)
-				/>
-				<label for="typeSub_{{$typeSub}}"
-					@class([
-						'radio-label',
-						'disabled' => $numLots == 0,
-					])>{{$desType}}
+                    <input class="filter_lot_list_js" id="typeSub_{{ $typeSub }}" name="typeSub" type="radio"
+                        value="{{ $typeSub }}" @checked($typeSub == $filters['typeSub']) @disabled($numLots == 0) />
+                    <label for="typeSub_{{ $typeSub }}" @class(['radio-label', 'disabled' => $numLots == 0])>{{ $desType }}
 
-					<span class="grid-count">
-					({{Tools::numberformat($numLots) }})
-					</span>
-				</label>
+                        <span class="grid-count">
+                            ({{ Tools::numberformat($numLots) }})
+                        </span>
+                    </label>
 
-			</div>
-		</div>
-		@endforeach
+                </div>
+            </div>
+        @endforeach
 
-	</div>
+    </div>
 </div>
