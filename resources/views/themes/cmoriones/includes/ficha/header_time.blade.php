60702e12d7c20999de377f510fa8d664f68101e7
Eloy N <enadal@labelgrup.com>
2023-11-23 09:39:38 +0100

    cmoriones change styles

--- resources/views/themes/cmoriones/includes/ficha/header_time.blade.php
+++ resources/views/themes/cmoriones/includes/ficha/header_time.blade.php
@@ -1,6 +1,6 @@
 <div class="ficha-info pb-3">
 
-	<div class="d-flex justify-content-between flex-wrap">
+	<div class="d-flex justify-content-between flex-wrap align-items-end">
 
 		<h4 class="info-type-auction mb-0">
 			{{ $lote_actual->name }}
