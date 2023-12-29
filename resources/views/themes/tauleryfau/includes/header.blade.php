99302ec76324908a7b598ef3cc9fa2ecf53f485e
Eloy N <enadal@labelgrup.com>
2023-12-29 11:44:59 +0100

    tauler change link translation funcionality in header

--- resources/views/themes/tauleryfau/includes/header.blade.php
+++ resources/views/themes/tauleryfau/includes/header.blade.php
@@ -1,9 +1,13 @@
 @php
+use App\libs\TradLib;
+
 $lang = Config::get('app.locale');
 $flagsLanguage = [
     'es' => 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAflBMVEX/AAD9AAD3AADxAADrAAD/eXn9bGz8YWH8WVn6UVH5SEj5Pz/3NDT0Kir9/QD+/nL+/lT18lDt4Uf6+j/39zD39yf19R3n5wDxflXsZ1Pt4Y3x8zr0wbLs1NXz8xPj4wD37t3jmkvsUU/Bz6nrykm3vJ72IiL0FBTyDAvhAABEt4UZAAAAX0lEQVR4AQXBQUrFQBBAwXqTDkYE94Jb73+qfwVRcYxVQRBRToiUfoaVpGTrtdS9SO0Z9FR9lVy/g5c99+dKl30N5uxPuviexXEc9/msC7TOkd4kHu/Dlh4itCJ8AP4B0w4Qwmm7CFQAAAAASUVORK5CYII=',
     'en' => 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAt1BMVEWSmb66z+18msdig8La3u+tYX9IaLc7W7BagbmcUW+kqMr/q6n+//+hsNv/lIr/jIGMnNLJyOP9/fyQttT/wb3/////aWn+YWF5kNT0oqz0i4ueqtIZNJjhvt/8gn//WVr/6+rN1+o9RKZwgcMPJpX/VFT9UEn+RUX8Ozv2Ly+FGzdYZrfU1e/8LS/lQkG/mbVUX60AE231hHtcdMb0mp3qYFTFwNu3w9prcqSURGNDaaIUMX5FNW5wYt7AAAAAjklEQVR4AR3HNUJEMQCGwf+L8RR36ajR+1+CEuvRdd8kK9MNAiRQNgJmVDAt1yM6kSzYVJUsPNssAk5N7ZFKjVNFAY4co6TAOI+kyQm+LFUEBEKKzuWUNB7rSH/rSnvOulOGk+QlXTBqMIrfYX4tSe2nP3iRa/KNK7uTmWJ5a9+erZ3d+18od4ytiZdvZyuKWy8o3UpTVAAAAABJRU5ErkJggg==',
 ];
+
+$urlToOtherLanguage = TradLib::getRouteTranslate(substr($_SERVER['REQUEST_URI'], 4), $lang, $lang == 'es' ? 'en' : 'es');
 
 function wpLink($code) {
 	$wpDomain = 'https://www.tauleryfau.com/';
@@ -95,7 +99,7 @@ function wpLink($code) {
                         @foreach (Config::get('app.locales') as $key => $value)
                             @if ($key != \Config::get('app.locale'))
                                 <a title="{{ trans("$theme-app.head.language_es") }}"
-                                    href="{{ "/$key" . \App\libs\TradLib::getRouteTranslate(substr($_SERVER['REQUEST_URI'], 4), \App::getLocale(), $key) }}">
+                                    href="{{ "/$key" . $urlToOtherLanguage }}">
 
                                     <div class="d-flex align-items-center justify-content-center">
                                         <img src="{{ $flagsLanguage[$key] }}" alt="{{ $key }}" width="16"
@@ -341,7 +345,7 @@ function wpLink($code) {
 							@if ($key != \Config::get('app.locale'))
 							<li>
 								<a title="{{ trans(\Config::get('app.theme') . '-app.head.language_' . $key) }}"
-									href="{{ "/$key" . \App\libs\TradLib::getRouteTranslate(substr($_SERVER['REQUEST_URI'], 4), \App::getLocale(), $key) }}">
+									href="{{ "/$key" . $urlToOtherLanguage }}">
 
 									<img src="{{ $flagsLanguage[$key] }}" alt="{{ \Config::get('app.locales')[$key] }}" width="16"
 									height="11" style="width: 16px; height: 11px; margin-right: 0.3em">
