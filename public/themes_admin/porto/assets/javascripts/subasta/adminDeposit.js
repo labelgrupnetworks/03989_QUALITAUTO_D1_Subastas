d3b51c457ce1d6ca80d810a98acf44e7aa4c8836
Eloy N <enadal@labelgrup.com>
2024-01-04 11:38:03 +0100

    admin refactor js to editing multiple deposits

--- public/themes_admin/porto/assets/javascripts/subasta/adminDeposit.js
+++ public/themes_admin/porto/assets/javascripts/subasta/adminDeposit.js
@@ -1,70 +1,63 @@
 $('#edit_multple_deposits').on('submit', function (event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    const formData = new FormData(edit_multple_deposits);
+	const formData = new FormData(edit_multple_deposits);
+	const isSelectAllDepositsChecked = (document.getElementById('selectAllDeposits')).checked;
 
-    const isSelectAllDepositsChecked = (document.getElementById('selectAllDeposits')).checked;
+	const url = isSelectAllDepositsChecked
+		? urlAllSelected.value
+		: edit_multple_deposits.action;
 
-    if (isSelectAllDepositsChecked) {
-        const urlSelected = document.getElementById('urlAllSelected').value;
+	isSelectAllDepositsChecked
+		? appendFiltersToFormData(formData)
+		: appendIdsToFormData(formData);
 
-		const searchParams = new URLSearchParams(window.location.search);
-		for (const param of searchParams) {
-			formData.append(param[0], param[1]);
-		}
-
-        $.ajax({
-            url: urlSelected,
-            type: "POST",
-			data: formData,
-            contentType: false,
-            processData: false,
-
-            success: function (result) {
-
-			$('#editMultpleDepositsModal').modal('hide');
-			saved('Archivos actualizados correctamente');
-
-			location.reload();
-
-            },
-            error: function () {
-
-                error();
-            }
-        });
-    } else {
-        const ids = selectedCheckItemsByName("desposit_ids");
-        ids.forEach(id => formData.append('ids[]', id));
-
-        $.ajax({
-			url: edit_multple_deposits.action,
-			type: "POST",
-			data: formData,
-			contentType: false,
-			processData: false,
-
-			success: function (result) {
-				$('#editMultpleDepositsModal').modal('hide');
-				saved('Archivos actualizados correctamente');
-
-				location.reload();
-			},
-			error: function () {
-
-				error();
-			}
-		});
-    }
-	
+	updataDepositStatus(url, formData);
 });
 
-$('input[name="desposit_ids"]').on('change', function () {
-    const isSelectAllDepositsChecked = (document.getElementById('selectAllDeposits')).checked;
+function appendFiltersToFormData(formData) {
+	const searchParams = new URLSearchParams(window.location.search);
+	const params = [...searchParams.entries()];
+	const cleanParams = params.filter(param => param[1] !== '');
 
-    if (isSelectAllDepositsChecked) {
-        document.getElementById('selectAllDeposits').checked = false;
-    }
+	// Add params to formData
+	cleanParams.forEach((entryParams) => {
+		const [key, value] = entryParams;
+		formData.append(key, value);
+	});
+}
+
+function appendIdsToFormData(formData) {
+	const ids = selectedCheckItemsByName("desposit_ids");
+	ids.forEach(id => formData.append('ids[]', id));
+}
+
+function updataDepositStatus(url, formData) {
+
+	$.ajax({
+		url,
+		type: "POST",
+		data: formData,
+		contentType: false,
+		processData: false,
+
+		success: function (result) {
+			$('#editMultpleDepositsModal').modal('hide');
+			saved('Depósitos actualizados correctamente');
+			location.reload();
+		},
+		error: function () {
+			error();
+		}
+	});
+}
+
+$('input[name="desposit_ids"]').on('change', function () {
+	const isSelectAllDepositsChecked = (document.getElementById('selectAllDeposits')).checked;
+
+	if (isSelectAllDepositsChecked) {
+		document.getElementById('selectAllDeposits').checked = false;
+	}
 });
 
 
