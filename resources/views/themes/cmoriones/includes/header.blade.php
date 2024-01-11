8388bb2b4b4986c5176978b6012e54b564193208
Eloy N <enadal@labelgrup.com>
2024-01-11 15:08:09 +0100

    cmoriones change logos sizes

--- resources/views/themes/cmoriones/includes/header.blade.php
+++ resources/views/themes/cmoriones/includes/header.blade.php
@@ -164,11 +164,11 @@
     <div class="container header-brand">
         <a class="navbar-brand" href="/{{ $lang }}" title="{{ \Config::get('app.name') }}">
             <img class="img-responsive" src="/themes/{{ $theme }}/assets/img/foto_logo_white.png"
-                alt="{{ \Config::get('app.name') }}" width="200">
+                alt="{{ \Config::get('app.name') }}" width="400">
         </a>
         <a class="navbar-brand" href="/{{ $lang }}" title="{{ \Config::get('app.name') }}">
             <img class="img-responsive logo-brand" src="/themes/{{ $theme }}/assets/img/logo_white.jpg"
-                alt="{{ \Config::get('app.name') }}" width="300">
+                alt="{{ \Config::get('app.name') }}" width="250">
         </a>
     </div>
 </section>
