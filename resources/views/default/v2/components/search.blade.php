5d553585597fa82eb7b4cf14353dd3c817030ec8
Eloy N <enadal@labelgrup.com>
2024-01-29 09:28:44 +0100

    cmoriones change dearch component and links

--- resources/views/default/v2/components/search.blade.php
+++ resources/views/default/v2/components/search.blade.php
@@ -1,12 +1,14 @@
 <div class="search-component-wrapper {{ $classes }} me-auto">
-	<form role="search" action="{{ \Routing::slug('busqueda') }}">
-		<div class="input-group">
-			<input class="form-control form-control-sm" type="search" name="texto" placeholder="{{ trans("$theme-app.head.search_label") }}" aria-label="{{ trans("$theme-app.head.search_label") }}" aria-describedby="button-addon2">
-			<button class="btn btn-sm btn-lb-primary d-flex align-items-center" type="submit">
-				<svg class="bi" width="16" height="16" fill="currentColor">
-					<use xlink:href="/bootstrap-icons.svg#search"></use>
-				</svg>
-			</button>
-		</div>
-	</form>
+    <form role="search" action="{{ $searchAction }}">
+        <div class="input-group">
+            <input class="form-control form-control-sm" name="description" type="search"
+                aria-label="{{ trans("$theme-app.head.search_label") }}" aria-describedby="button-addon2"
+                placeholder="{{ trans("$theme-app.head.search_label") }}">
+            <button class="btn btn-sm btn-lb-primary d-flex align-items-center" type="submit">
+                <svg class="bi" width="16" height="16" fill="currentColor">
+                    <use xlink:href="/bootstrap-icons.svg#search"></use>
+                </svg>
+            </button>
+        </div>
+    </form>
 </div>
