0cde7fcbd109f3ddaf744bbd903e9d3cc3470f2b
Eloy N <enadal@labelgrup.com>
2024-02-05 13:38:13 +0100

    cmoriones change design ficha

--- resources/views/themes/cmoriones/includes/ficha/custom_sections.blade.php
+++ resources/views/themes/cmoriones/includes/ficha/custom_sections.blade.php
@@ -6,23 +6,6 @@
     $caracteristicas = App\Models\V5\FgCaracteristicas_Hces1::getByLot($lote_actual->num_hces1, $lote_actual->lin_hces1);
 @endphp
 
-@if (in_array($lote_actual->cod_sub, ['CEREMATE', 'COMDEUDA', 'REOS']))
-    <section class="ficha-login">
-        <p class="mb-2">
-            Si quieres tener una VALORACIÓN REAL de este activo a fecha de hoy por el que muestras interés de forma
-            TOTALMENTE GRATUITA, ve a <button class="btn btn-link btn_login p-0">login</button> completa los datos y
-            solicita más información. Uno de nuestros expertos
-            valorará el activo con el máximo detalle y precisión y se pondrá en contacto contigo para transmitirte el
-            resultado de la valoración.
-        </p>
-        @if (Session::has('user'))
-            <button class="btn btn-lb-primary w-100">
-                Solicitar valoración
-            </button>
-        @endif
-    </section>
-@endif
-
 <section class="ficha-contact">
     <a class="btn btn-success"
         href="https://wa.me/34602252061?text=Estoy%20interesado%20es%20el%20siguiente%20activo%20{{ URL::current() }}"
--- resources/views/themes/cmoriones/includes/ficha/share.blade.php
+++ resources/views/themes/cmoriones/includes/ficha/share.blade.php
@@ -3,6 +3,8 @@
 	$linkToShare = URL::current();
 @endphp
 
+
+
 <div class="zone-share-social d-flex justify-content-between">
     <p class="shared">{{ trans(\Config::get('app.theme') . '-app.lot.share_lot') }}</p>
 
@@ -61,4 +63,46 @@
         </li>
     </ul>
 
+</div>
+
+@if (in_array($lote_actual->cod_sub, ['CEREMATE', 'COMDEUDA', 'REOS']))
+    <section class="ficha-login">
+        <p class="mb-2">
+            Si quieres tener una VALORACIÓN REAL de este activo a fecha de hoy por el que muestras interés de forma
+            TOTALMENTE GRATUITA, ve a <button class="btn btn-link btn_login p-0">login</button> completa los datos y
+            solicita más información. Uno de nuestros expertos
+            valorará el activo con el máximo detalle y precisión y se pondrá en contacto contigo para transmitirte el
+            resultado de la valoración.
+        </p>
+        @if (Session::has('user'))
+            <button class="btn btn-lb-primary w-100">
+                Solicitar valoración
+            </button>
+        @endif
+    </section>
+@endif
+
+@php
+    $previuos = $data['previous'];
+    $next = $data['next'];
+@endphp
+
+<div class="d-flex align-content-center">
+    @if ($previuos)
+        <div class="btn-group me-auto">
+            <a class="btn btn-lb-primary d-flex align-items-center" href="{{ $previuos }}">
+                @include('components.boostrap_icon', ['icon' => 'chevron-left'])
+            </a>
+            <a class="btn btn-light" href="{{ $previuos }}">{{ trans("$theme-app.subastas.last") }}</a>
+        </div>
+    @endif
+
+    @if ($next)
+	<div class="btn-group ms-auto">
+			<a class="btn btn-light" href="{{ $next }}">{{ trans("$theme-app.subastas.next") }}</a>
+            <a class="btn btn-lb-primary d-flex align-items-center" href="{{ $next }}">
+                @include('components.boostrap_icon', ['icon' => 'chevron-right'])
+            </a>
+        </div>
+    @endif
 </div>
