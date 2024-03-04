d8589ff7b344dcf706288f6cc7e10d30cef03983
llandeira <llandeira@labelgrup.com>
2024-03-04 17:18:45 +0100

    JESUSVICO - Slimming down the next and previous buttons in Lot's ficha

--- resources/views/themes/jesusvico/includes/ficha/previous_next.blade.php
+++ resources/views/themes/jesusvico/includes/ficha/previous_next.blade.php
@@ -2,10 +2,15 @@
 	$previuos = $data['previous'];
 	$next = $data['next'];
 
-	$searchUrl = Tools::url_lot_to_js($lote_actual->cod_sub, $lote_actual->id_auc_sessions, $lote_actual->ref_asigl0, $lote_actual->num_hces1);
+	$searchUrl = Tools::url_lot_to_js(
+	    $lote_actual->cod_sub,
+	    $lote_actual->id_auc_sessions,
+	    $lote_actual->ref_asigl0,
+	    $lote_actual->num_hces1,
+	);
 @endphp
 
-<div class="d-flex align-content-center">
+<div class="d-flex align-content-center" style="height: 40px;">
 	@if ($previuos)
 		<div class="btn-group me-auto">
 			<a class="btn btn-lb-primary d-flex align-items-center" href="{{ $previuos }}">
@@ -15,19 +20,18 @@
 		</div>
 	@endif
 
-	<div class="search-lot-container d-none d-md-block">
+	<div class="btn-group search-lot-container d-none d-md-block">
 		<form id="searchLot" class="form-single-lot samsung-visual" method="get" action="{{ $searchUrl }}">
-			<div class="bar-filters into-lot flex">
 
-				<div class="input-group">
-					<input class="form-control control-number" type="number" name="reference">
-					<button id="single-lot" class="btn btn-lb-primary"
-						type="submit">
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 20 20">
-							<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-						  </svg>
-					</button>
-				</div>
+			<div class="input-group">
+				<input class="form-control control-number search-lot-bar-input" type="number" name="reference">
+				<button id="single-lot" class="btn btn-lb-primary search-lot-bar-button" type="submit">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search"
+						viewBox="0 0 20 20">
+						<path
+							d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+					</svg>
+				</button>
 
 			</div>
 
