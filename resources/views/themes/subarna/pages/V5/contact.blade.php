d44492d38a030b1d58489a35f3fc756c6acf21f4
llandeiralabelgrup <llandeira@labelgrup.com>
2024-04-08 11:31:54 +0200

    SUBARNA - Add photo preview in contact page

--- resources/views/themes/subarna/pages/V5/contact.blade.php
+++ resources/views/themes/subarna/pages/V5/contact.blade.php
@@ -23,7 +23,8 @@
 						</h1>
 					</div>
 					<div class="col-xs-6">
-						<img class="header-image height-image img-responsive" src="/themes/{{ $theme }}/assets/img/top_banner_image_rounded.png">
+						<img class="header-image height-image img-responsive"
+							src="/themes/{{ $theme }}/assets/img/top_banner_image_rounded.png">
 					</div>
 				</div>
 			</div>
@@ -157,15 +158,19 @@
 											Tamaño total, no superior a 20MB.</p>
 									</div>
 								</div>
-								<input class="hidden" id="files" type="file" accept="image/png, image/jpeg" name="imagen[]" multiple="" required="">
+								<div class="contact-images-preview">
+
+								</div>
+								<input class="hidden" id="files" type="file" accept="image/png, image/jpeg" name="imagen[]" multiple=""
+									required="">
 							</div>
 						</div>
 						<div class="col-xs-12">
 							<div class="captcha-and-terms">
 								<div class="check_term">
-										<input type="checkbox" class="newsletter" name="condiciones" value="on" id="bool__1__condiciones"
-											autocomplete="off">
-										<label for="bool__1__condiciones"><?= trans($theme . '-app.emails.privacy_conditions') ?>
+									<input type="checkbox" class="newsletter" name="condiciones" value="on" id="bool__1__condiciones"
+										autocomplete="off">
+									<label for="bool__1__condiciones"><?= trans($theme . '-app.emails.privacy_conditions') ?>
 								</div>
 
 								<div class="g-recaptcha" data-sitekey="{{ \Config::get('app.codRecaptchaEmailPublico') }}"
@@ -182,5 +187,84 @@
 			</div>
 		</div>
 	</div>
+
+	<script>
+		const input = document.querySelector("input[type=file]#files");
+		const preview = document.querySelector(".contact-images-preview");
+
+		input.addEventListener("change", updateImageDisplay);
+
+		function updateImageDisplay() {
+			while (preview.firstChild) {
+				preview.removeChild(preview.firstChild);
+			}
+
+			const curFiles = input.files;
+			if (curFiles.length === 0) {
+				const para = document.createElement("p");
+				para.textContent = "No files currently selected for upload";
+				preview.appendChild(para);
+			} else {
+				const list = document.createElement("ol");
+				preview.appendChild(list);
+
+				for (const file of curFiles) {
+					const listItem = document.createElement("li");
+					const para = document.createElement("p");
+					if (validFileType(file)) {
+						para.textContent = `File name ${file.name}, file size ${returnFileSize(
+          					file.size,
+        				)}.`;
+						const image = document.createElement("img");
+						image.src = URL.createObjectURL(file);
+						image.alt = image.title = file.name;
+
+						listItem.appendChild(image);
+						listItem.appendChild(para);
+					} else {
+						para.textContent = `File name ${file.name}: Not a valid file type. Update your selection.`;
+						listItem.appendChild(para);
+					}
+
+					list.appendChild(listItem);
+				}
+			}
+		}
+
+		const fileTypes = [
+			"image/apng",
+			"image/bmp",
+			"image/gif",
+			"image/jpeg",
+			"image/pjpeg",
+			"image/png",
+			"image/svg+xml",
+			"image/tiff",
+			"image/webp",
+			"image/x-icon",
+		];
+
+		function validFileType(file) {
+			return fileTypes.includes(file.type);
+		}
+
+		function returnFileSize(number) {
+			if (number < 1024) {
+				return `${number} bytes`;
+			} else if (number >= 1024 && number < 1048576) {
+				return `${(number / 1024).toFixed(1)} KB`;
+			} else if (number >= 1048576) {
+				return `${(number / 1048576).toFixed(1)} MB`;
+			}
+		}
+
+		const button = document.querySelector("form button");
+		button.addEventListener("click", (e) => {
+			e.preventDefault();
+			const para = document.createElement("p");
+			para.append("Image uploaded!");
+			preview.replaceChildren(para);
+		});
+	</script>
 
 @stop
