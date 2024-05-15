6f53ff4efb44895fc0dcd4aec430e80c82b9dede
Eloy N <enadal@labelgrup.com>
2024-05-15 08:49:49 +0200

    Global - Remove dependency injection in AuctionInTime

--- app/Notifications/AuctionInTime.php
+++ app/Notifications/AuctionInTime.php
@@ -101,7 +101,7 @@ class AuctionInTime extends Notification implements ShouldQueue
 		};
 	}
 
-	private function whenTimeBody(string $whenTime, Fgsub $auction)
+	private function whenTimeBody(string $whenTime, $auction)
 	{
 		return match ($whenTime) {
 			'week' => "La subasta {$auction->name} se realizará en 1 semana",
