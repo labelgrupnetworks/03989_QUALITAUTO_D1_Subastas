7276ce7f2a5ec99be50528cdbdbc82827a98b068
Eloy N <enadal@labelgrup.com>
2024-05-24 12:58:29 +0200

    Subarna - Change gtag with consent

--- resources/views/themes/subarna/includes/google_head.blade.php
+++ resources/views/themes/subarna/includes/google_head.blade.php
@@ -5,20 +5,34 @@
     Config::set('app.cookies.analysis', [Cookies::THIRD_GOOGLE, Cookies::THIRD_HOTJAR, Cookies::THIRD_LIVEAGENT]);
 @endphp
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-463062636"></script>
-
-@if($cookiesPreferences->isAnalysisAllowed())
-<!-- Global site tag (gtag.js) - Google Analytics -->
-{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=AW-463062636"></script> --}}
 <script>
-    window.dataLayer = window.dataLayer || [];
+	// Define dataLayer and the gtag function.
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+</script>
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+@if (!$cookiesPreferences->isAnalysisAllowed() )
+	<script>
+		// Set default consent to 'denied' as a placeholder
+		// Determine actual values based on your own requirements
+		gtag('consent', 'default', {
+			'ad_storage': 'denied',
+			'ad_user_data': 'denied',
+			'ad_personalization': 'denied',
+			'analytics_storage': 'denied'
+		});
 
-    gtag('config', 'AW-463062636');
+		ga = () => {};
+	</script>
+@else
+<script>
+	gtag('consent', 'default', {
+			'ad_storage': 'granted',
+			'ad_user_data': 'granted',
+			'ad_personalization': 'denied',
+			'analytics_storage': 'granted'
+		});
+
 </script>
 
 <!-- Hotjar Tracking Code for www.subarna.net -->
@@ -38,9 +52,12 @@
         a.appendChild(r);
     })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 </script>
-@else
-<script>
-	ga = () => {};
-	gtag = () => {};
-</script>
 @endif
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-463062636"></script>
+<script>
+	gtag('js', new Date());
+	gtag('config', 'AW-463062636');
+</script>
+
