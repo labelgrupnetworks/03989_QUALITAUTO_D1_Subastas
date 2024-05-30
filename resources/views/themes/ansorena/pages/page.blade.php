8760cad19df4732098b2f605ba21e35e0c20d504
llandeiralabelgrup <llandeira@labelgrup.com>
2024-05-30 08:44:07 +0200

    ANSO - Create whatsapp button and put in jewery pages

--- resources/views/themes/ansorena/pages/page.blade.php
+++ resources/views/themes/ansorena/pages/page.blade.php
@@ -6,10 +6,13 @@
 
 @section('content')
 
-
 @if ($data['data']->manageable_web_page == 2)
         @include('pages.page-v2')
     @else
         @include('pages.page-v1')
     @endif
 @stop
+
+@if ($data['data']->id_web_page == '96' || $data['data']->id_web_page == '97')
+	@include('includes.whatsapp_button', ['whaPhone' => '34600000000'])
+@endif
