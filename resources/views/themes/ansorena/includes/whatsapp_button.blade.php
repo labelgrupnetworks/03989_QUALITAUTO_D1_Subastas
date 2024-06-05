ebeb50135d3975e7ab1c0a59548e8cf072018f0b
llandeiralabelgrup <llandeira@labelgrup.com>
2024-06-05 10:23:37 +0200

    ANSO - Put translation in whatsapp button

--- resources/views/themes/ansorena/includes/whatsapp_button.blade.php
+++ resources/views/themes/ansorena/includes/whatsapp_button.blade.php
@@ -1,6 +1,6 @@
 @if (!empty(config('app.whatsapp_num_button')))
 	<a class="whatsapp-button" href="https://api.whatsapp.com/send?phone={{ config('app.whatsapp_num_button') }}" target="_blank">
 		<i class="fa fa-whatsapp"></i>
-		<span>Le ayudaremos</span>
+		<span>{{ trans("$theme-app.foot.whatsapp_button") }}</span>
 	</a>
 @endif
