b791a3a497fe2416dd9d59dd38fdb79466d1c080
Eloy N <enadal@labelgrup.com>
2024-09-03 12:59:13 +0200

    Segre - Fix capthca in ask-info-lot

--- resources/views/themes/segre/includes/ficha/pujas_ficha_V.blade.php
+++ resources/views/themes/segre/includes/ficha/pujas_ficha_V.blade.php
@@ -26,7 +26,6 @@ $importe = $importe + $comision +  $ivaComision;
 $importeExchange =\Tools::moneyFormat( $importe);
 $importe =  \Tools::moneyFormat($importe,false,2);
 ?>
-<script src="https://www.google.com/recaptcha/api.js?hl={{ \Config::get('app.locale') }}" async defer></script>
 <div class="col-xs-12 no-padding ">
 
 
@@ -64,6 +63,7 @@ $importe =  \Tools::moneyFormat($importe,false,2);
 		<div class="col-xs-12">
 			<p class="pre-title-principal adj-text">	{{ trans(\Config::get('app.theme').'-app.galery.request_information') }} </p>
 					<form name="infoLotForm" id="infoLotForm" method="post" action="javascript:sendInfoLot()">
+						<input type="hidden" data-sitekey="{{ config('app.captcha_v3_public') }}" name="captcha_token" value="">
 						<input type="hidden" name="auction" value="{{ $lote_actual->cod_sub}} - {{ $lote_actual->des_sub}}">
 						<input type="hidden" name="lot" value="   {{$lote_actual->descweb_hces1 }} ">
 
@@ -91,39 +91,19 @@ $importe =  \Tools::moneyFormat($importe,false,2);
 								<textarea  class="form-control  " name="comentario"  id="textogrande__1__comentario"   rows="10">  </textarea>
 
 							</div>
-							@php
-								/*
-
-							<div class="check_term col-xs-12 mt-2">
-								<div class="row">
-									<div class="col-xs-2 col-md-1">
-										<input type="checkbox" class="newsletter" name="condiciones" value="on"
-											id="bool__1__condiciones" autocomplete="off">
-									</div>
-									<div class="col-xs-10 col-md-11">
-										<label
-											for="accept_new"><?= trans(\Config::get('app.theme') . '-app.emails.privacy_conditions') ?></label>
-									</div>
-								</div>
-							</div>
-							*/
-							@endphp
 							<div class="col-xs-12">
 								<p>Los campos con * son obligatorios </p>
 							</div>
 							<div class="col-xs-12 mt">
-								<div class="row">
-									<div class="g-recaptcha col-xs-6"
-										data-sitekey="{{\Config::get('app.codRecaptchaEmailPublico')}}"
-										data-callback="onSubmit">
-									</div>
-								</div>
+								<p class="captcha-terms">
+									{!! trans("$theme-app.global.captcha-terms") !!}
+								</p>
 							</div>
 
 							<div class="col-xs-12 mt-3 mb-3">
 								<div class="row">
 									<div class="col-xs-6">
-										<a onclick="javascript:submit_form(document.getElementById('infoLotForm'),0);" class="button-principal submitButton">Enviar</a>
+										<a onclick="javascript:submit_form(document.getElementById('infoLotForm'),0);" class="btn button-principal submitButton">Enviar</a>
 									</div>
 								</div>
 							</div>
