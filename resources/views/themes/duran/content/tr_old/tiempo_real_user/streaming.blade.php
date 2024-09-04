a7e57776015996f632d0697270a183b289317a4c
Eloy N <enadal@labelgrup.com>
2024-09-04 15:27:09 +0200

    Duran - Remove old or copy blades

--- resources/views/themes/duran/content/tr_old/tiempo_real_user/streaming.blade.php
+++
@@ -1,4 +0,0 @@
-<p class="delay">{{ trans($theme.'-app.sheet_tr.delay') }}</p>
-<div class="streaming">
-    <iframe src="//stream01.labelgrup.com:5443/WebRTCAppEE/play.html?name=273039929349767838886597" height="100%" width="auto" frameborder="0" allowfullscreen></iframe>
-</div>
