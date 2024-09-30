d0b867b767fe33130b978e3051c526de310406ea
Eloy N <enadal@labelgrup.com>
2024-09-30 15:31:25 +0200

    Ansorena - Change proyector2 design

---
+++ resources/views/themes/ansorena/includes/tr/tiempo_real_proyector/v2/header.blade.php
@@ -0,0 +1,19 @@
+@php
+	$refFormat = str_replace(['.1', '.2', '.3', '.4', '.5'], ['-A', '-B', '-C', '-D', '-E'], $data['subasta_info']->lote_actual->ref_asigl0);
+@endphp
+
+<header class="enc">
+
+	<div class="lot">
+		<p id="lote_actual_main">
+			{{ trans("$theme-app.sheet_tr.lot") }}
+			<span id="info_lot_actual">{{ $refFormat }}</span>
+		</p>
+	</div>
+
+	<a class="brand" href="{{ \Routing::slug('/') }}">
+        <img class="img-responsive" src="{{ $img_url }}/logo.png">
+    </a>
+
+	<h1 class="auction_number">{{ $data['name'] }}</h1>
+</header>
