ca592b45a634ab15a83239dcb271d5ce5c2ba892
Eloy N <enadal@labelgrup.com>
2024-11-12 15:19:37 +0100

    Magna - Add console command to check if cron is active

---
+++ app/Console/Commands/Observability/CheckCronIsActive.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands\Observability;
+
+use App\Actions\Observability\CheckFailedJobsAction;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class CheckFailedJobs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'label:check-cron-is-active';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check if the cron is active';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+		Log::debug("Cron is active!!!!");
+    }
+}
