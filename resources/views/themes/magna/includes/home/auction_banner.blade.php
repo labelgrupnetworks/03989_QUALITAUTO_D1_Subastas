0d770d39e980e25188bbf3be676f84c2b19ce840
Eloy N <enadal@labelgrup.com>
2024-11-13 11:01:08 +0100

    Magna - Add date to home auction

--- resources/views/themes/magna/includes/home/auction_banner.blade.php
+++ resources/views/themes/magna/includes/home/auction_banner.blade.php
@@ -1,6 +1,13 @@
 @php
     use App\Models\V5\AucSessionsFiles;
+    use Carbon\Carbon;
+
     $emp = Config::get('app.emp');
+
+    $dateFormat = Carbon::parse($auction->session_start)
+        ->locale(config('app.locale'))
+        ->isoFormat('LL');
+
     $auctionImage = Tools::urlAssetsCache("/img/AUCTION_{$emp}_{$auction->cod_sub}.jpg");
 
     $url_lotes = Tools::url_auction($auction->cod_sub, $auction->name, $auction->id_auc_sessions, $auction->reference);
@@ -12,16 +19,17 @@
         <div class="col-12 col-md-5 auction-banner-info-column">
             <div>
                 <h1>{{ $auction->name }}</h1>
+				<h3>{{ $dateFormat }}</h3>
 
                 <a class="btn btn-outline-lb-primary rounded-5" href="{{ $url_lotes }}">
-                    Ir a la subasta
+					{{ trans("$theme-app.subastas.go_to_the_auction") }}
                 </a>
 
-				@if($catalogLink)
-                <a class="btn btn-outline-lb-primary rounded-5" href="{{ $catalogLink->url }}" target="_blank">
-                    Ver catálogo
-                </a>
-				@endif
+                @if ($catalogLink)
+                    <a class="btn btn-outline-lb-primary rounded-5" href="{{ $catalogLink->url }}" target="_blank">
+                        {{ trans("$theme-app.subastas.see_catalogue") }}
+                    </a>
+                @endif
             </div>
         </div>
         <div class="col-12 col-md-7">
