31a193a9c47800be6c40bd04198a2e517da2a795
Eloy N <enadal@labelgrup.com>
2024-11-26 11:29:33 +0100

    Magna - Change cookies modal

---
+++ resources/views/themes/magna/includes/cookie.blade.php
@@ -0,0 +1,35 @@
+@php
+    $style = $style ?? 'popover'; // popover | bar
+@endphp
+
+<div class="cookies" data-style="{{ $style }}">
+    <p class="cookies__title">{{ trans("$theme-app.cookies.cookies_policy") }}</p>
+
+    <div class="cookies_content">
+        <div class="cookies__messages">
+            <p class="cookies__message">
+                {{ trans("$theme-app.cookies.popover_description") }}
+            </p>
+
+            <p class="cookies__message">
+                {{ trans("$theme-app.cookies.popover_description2") }}
+            </p>
+        </div>
+
+        <div class="cookies_buttons">
+            <button onclick="acceptAllCookies()">
+                {{ trans("$theme-app.cookies.accept_all") }}
+            </button>
+            <button type="button" data-toggle="modal" data-bs-toggle="modal" data-target="#cookiesPersonalize" data-bs-target="#cookiesPersonalize">
+                {{ trans("$theme-app.cookies.personalize") }}
+            </button>
+            <button onclick="rejectAllCookies()">
+                {{ trans("$theme-app.cookies.reject_all") }}
+            </button>
+			<a href="{{ Routing::translateSeo('pagina') . trans("$theme-app.links.cookies") }}">
+				{{ trans("$theme-app.login_register.more_info") }}
+			</a>
+        </div>
+    </div>
+
+</div>
