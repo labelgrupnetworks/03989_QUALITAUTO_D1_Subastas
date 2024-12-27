45b878640048c69b72c905400f80c8ae03649d9f
Eloy N <enadal@labelgrup.com>
2024-12-27 14:31:47 +0100

    Ansorena - Add content to video banner

--- resources/views/themes/ansorena/includes/banners/video.blade.php
+++ resources/views/themes/ansorena/includes/banners/video.blade.php
@@ -1,6 +1,18 @@
 @if ($banner)
     @foreach ($banner->activeItems as $bannerItem)
         <div class="video-banner video-banner-{{ $bannerItem->id }}">
+            @if (!empty($content['title']))
+                <div class="video-banner-content">
+                    <h2 class="video-banner-title">{{ $content['title'] }}</h2>
+                    <h3 class="video-banner-subtitle">
+                        {{ trans("$theme-app.subastas.lot_subasta_online") }}
+                    </h3>
+                    <a class="video-banner-url" href="{{ route('subastas.online') }}">
+						{{ trans("$theme-app.lot_list.go_to_auction") }}
+					</a>
+                </div>
+            @endif
+
             <video class="w-100" src="{{ $bannerItem->texto }}" controls autoplay muted loop playsinline>
                 Tu navegador no admite el elemento <code>video</code>.
             </video>
