c8db1f95142194ae995cbcf4569707579936ae90
RsanchezLabelGrup <rsanchez@labelgrup.com>
2025-01-02 12:14:29 +0100

    ansorena enlace subasta actual online

--- resources/views/themes/ansorena/includes/banners/video.blade.php
+++ resources/views/themes/ansorena/includes/banners/video.blade.php
@@ -7,7 +7,7 @@
                     <h3 class="video-banner-subtitle">
                         {{ trans("$theme-app.subastas.lot_subasta_online") }}
                     </h3>
-                    <a class="btn btn-medium btn-outline-lb-translucent video-banner-btn" href="{{ route('subastas.online') }}">
+                    <a class="btn btn-medium btn-outline-lb-translucent video-banner-btn" href="{{ route('subasta.actual-online') }}">
 						{{ trans("$theme-app.lot_list.go_to_auction") }}
 					</a>
                 </div>
