d23fa9b442bc8b423a81d6d557a9ea3a674e75f7
Eloy N <enadal@labelgrup.com>
2025-03-25 15:01:56 +0100

    Duran - Change streming address

--- resources/views/themes/duran/content/tr/tiempo_real_user/streaming.blade.php
+++ resources/views/themes/duran/content/tr/tiempo_real_user/streaming.blade.php
@@ -1,4 +1,4 @@
 <p class="delay">{{ trans($theme.'-app.sheet_tr.delay') }}</p>
 <div class="streaming">
-	<iframe width="560" height="315" src="//stream01.labelgrup.com:5443/Stream_Duran/play.html?name=190829310155942612998042" frameborder="0" allowfullscreen></iframe>
+	<iframe width="560" height="315" src="//stream01.labelgrup.com:5443/Stream_Duran/play.html?name=258488059507884966761074" frameborder="0" allowfullscreen></iframe>
 </div>
