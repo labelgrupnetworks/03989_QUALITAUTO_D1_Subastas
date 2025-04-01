881e72e85639f6ddcffad305856e35598ca386d1
Eloy N <enadal@labelgrup.com>
2025-04-01 08:49:14 +0200

    Soler - Change get old resource banner from main_emp

--- app/Services/Content/BannerService.php
+++ app/Services/Content/BannerService.php
@@ -17,12 +17,11 @@ class BannerService
 	{
 		$theme = Config::get('app.theme');
 		$main_emp = Config::get('app.main_emp');
-		$emp = Config::get('app.emp');
 
 		$keyCache = "content.slider.{$keyBanner}.{$theme}.{$main_emp}";
 		$timeToCache = Config::get('app.time_cache', 180);
 
-		return Cache::remember($keyCache, $timeToCache, function() use ($keyBanner, $emp) {
+		return Cache::remember($keyCache, $timeToCache, function() use ($keyBanner, $main_emp) {
 			return DB::table('WEB_BANNER')
 				->join('WEB_RESOURCE_BANNER', 'WEB_RESOURCE_BANNER.ID_WEB_BANNER', '=', 'WEB_BANNER.ID_WEB_BANNER')
 				->join('WEB_RESOURCE', 'WEB_RESOURCE.ID_WEB_RESOURCE', '=', 'WEB_RESOURCE_BANNER.ID_WEB_RESOURCE')
@@ -30,7 +29,7 @@ class BannerService
 				->where('WEB_BANNER.KEY_NAME', $keyBanner)
 				->where('WEB_BANNER.ENABLED', 1)
 				->where('WEB_RESOURCE.ENABLED', 1)
-				->where('WEB_BANNER.ID_EMP', $emp)
+				->where('WEB_BANNER.ID_EMP', $main_emp)
 				->orderBy('WEB_RESOURCE_BANNER.ORDEN', 'asc')
 				->limit(10)
 				->get();
