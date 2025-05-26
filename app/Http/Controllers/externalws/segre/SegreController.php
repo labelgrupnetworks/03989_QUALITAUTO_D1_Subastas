b45ee93379d200a79376d3448aece84b7a7d8044
Eloy N <enadal@labelgrup.com>
2025-05-26 12:54:47 +0200

    Segre - Add Angeles in errors webservice notifications and indent file

--- app/Http/Controllers/externalws/segre/SegreController.php
+++ app/Http/Controllers/externalws/segre/SegreController.php
@@ -1,83 +1,79 @@
 <?php
+
 namespace App\Http\Controllers\externalws\Segre;
 
-use Request;
 use Exception;
-use stdClass;
-
-use SimpleXMLElement;
 use App\libs\EmailLib;
-use Hamcrest\Arrays\IsArray;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 
 class SegreController extends Controller
 {
-	public function QueueCall($parameters, $function){
+	public function QueueCall($parameters, $function)
+	{
 
 		return $this->callWebService($parameters, $function);
 	}
 
-	function prueba( $parameters, $function){
-		$this->callWebService( $parameters, $function);
+	function prueba($parameters, $function)
+	{
+		$this->callWebService($parameters, $function);
 	}
 
-	protected function callWebService( $parameters, $function){
+	protected function callWebService($parameters, $function)
+	{
 
-		try{
+		try {
 
 			$user = "UsLabelGrup";
-			$password="MyPassw01!!";
-			$method= "POST";
-			$url = \Config::get("app.SegreURLWebservice")."/".$function;
+			$password = "MyPassw01!!";
+			$method = "POST";
+			$url = Config::get("app.SegreURLWebservice") . "/" . $function;
 
 			$clientGuzz = new Client();
-
-
-			$response = $clientGuzz->request($method, $url,[
+			$response = $clientGuzz->request($method, $url, [
 				'headers' => [
 					'Content-Type' => 'text/plain',
 					'Accept' => '*/*',
-				  ],
-				   'auth' => [$user, $password],
-				   'verify' => false,
-				   'body' => $parameters
+				],
+				'auth' => [$user, $password],
+				'verify' => false,
+				'body' => $parameters
 
-				]);
-				if(empty($response) || empty($response->getBody())){
-					$this->ErrorLog($function ,json_encode($parameters), "");
-					$this->sendEmailError($function,htmlspecialchars(json_encode($parameters)),"", true );
-					return;
-				}
-				$body = json_decode($response->getBody());
+			]);
+			if (empty($response) || empty($response->getBody())) {
+				$this->ErrorLog($function, json_encode($parameters), "");
+				$this->sendEmailError($function, htmlspecialchars(json_encode($parameters)), "", true);
+				return;
+			}
+			$body = json_decode($response->getBody());
 
 
 			return $body;
-
-
-		}catch (Exception $e){
-			\Log::info($e);
+		} catch (Exception $e) {
+			Log::info($e);
 			# de momento no enviamos a Anselmo, si no a subastas
-			$this->sendEmailError($function,print_r($parameters, true), $e->getMessage(),false );
+			$this->sendEmailError($function, print_r($parameters, true), $e->getMessage(), false);
 		}
-
 	}
 
-	protected function  ErrorLog($function, $stringXML, $info ){
-		\Log::info("Error web Service  Segre,  funcion $function,  info $info".$stringXML);
-
+	protected function  ErrorLog($function, $stringXML, $info)
+	{
+		Log::info("Error web Service  Segre,  funcion $function,  info $info" . $stringXML);
 	}
 
-
-	protected function sendEmailError($function,$request, $response, $sendExternalAdmin = false ){
-
+	protected function sendEmailError($function, $request, $response, $sendExternalAdmin = false)
+	{
 		$email = new EmailLib('WS_ERROR');
-		if(!empty($email->email)){
+		if (!empty($email->email)) {
 
-			if($sendExternalAdmin){
+			if ($sendExternalAdmin) {
 				$email->setTo("aizard@aquilum.es");
+				$email->setCc('angeles.fn@subastassegre.es');
 				$email->setBcc("enadal@labelgrup.com");
-			}else{
+			} else {
 				$email->setTo("enadal@labelgrup.com");
 				$email->setBcc("subastas@labelgrup.com");
 			}
@@ -87,9 +83,4 @@ class SegreController extends Controller
 			$email->send_email();
 		}
 	}
-
-
-
-
-
 }
