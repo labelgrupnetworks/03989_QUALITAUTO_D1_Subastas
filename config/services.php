9917a1c2455cb2119c997d3975991115fb5267b3
Eloy N <enadal@labelgrup.com>
2025-06-12 07:53:57 +0200

    Global - Add Microsoft Teams notification support

--- config/services.php
+++ config/services.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Third Party Services
     |--------------------------------------------------------------------------
@@ -14,22 +14,25 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
-    ],
+	'mailgun' => [
+		'domain' => env('MAILGUN_DOMAIN'),
+		'secret' => env('MAILGUN_SECRET'),
+		'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+		'scheme' => 'https',
+	],
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
+	'postmark' => [
+		'token' => env('POSTMARK_TOKEN'),
+	],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
+	'ses' => [
+		'key' => env('AWS_ACCESS_KEY_ID'),
+		'secret' => env('AWS_SECRET_ACCESS_KEY'),
+		'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+	],
+	'microsoft_teams' => [
+		'webhook_url' => env('TEAMS_WEBHOOK_URL', null),
+	],
 	'google' => [
 		'client_id' => env('GOOGLE_CLIENT_ID'),
 		'client_secret' => env('GOOGLE_CLIENT_SECRET'),
