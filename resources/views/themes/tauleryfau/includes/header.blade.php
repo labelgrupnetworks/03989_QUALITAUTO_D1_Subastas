754ec527bf21250074b2ccaafa2e73eca0a8c1e2
Eloy N <enadal@labelgrup.com>
2025-06-16 10:05:46 +0200

    Tauler - Update header web

--- resources/views/themes/tauleryfau/includes/header.blade.php
+++ resources/views/themes/tauleryfau/includes/header.blade.php
@@ -2,6 +2,7 @@
     use App\libs\TradLib;
 
     $lang = Config::get('app.locale');
+	$langName = Config::get('app.locales')[$lang];
     $flagsLanguage = [
         'es' =>
             'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAMAAABBPP0LAAAAflBMVEX/AAD9AAD3AADxAADrAAD/eXn9bGz8YWH8WVn6UVH5SEj5Pz/3NDT0Kir9/QD+/nL+/lT18lDt4Uf6+j/39zD39yf19R3n5wDxflXsZ1Pt4Y3x8zr0wbLs1NXz8xPj4wD37t3jmkvsUU/Bz6nrykm3vJ72IiL0FBTyDAvhAABEt4UZAAAAX0lEQVR4AQXBQUrFQBBAwXqTDkYE94Jb73+qfwVRcYxVQRBRToiUfoaVpGTrtdS9SO0Z9FR9lVy/g5c99+dKl30N5uxPuviexXEc9/msC7TOkd4kHu/Dlh4itCJ8AP4B0w4Qwmm7CFQAAAAASUVORK5CYII=',
@@ -23,6 +24,43 @@
     }
 @endphp
 <header class="header-web">
+    <div class="topbar">
+		@if(!Session::has('user'))
+		<a data-toggle="modal" data-target="#modalLogin" role="button" title="{{ trans("web.login_register.login") }}">
+			{{ trans("web.login_register.login") }}
+		</a>
+		@else
+		<a href="{{ route('panel.summary', ['lang' => config('app.locale')]) }}">{{ trans("web.login_register.my_panel") }}</a>
+
+			@if(Session::get('user.admin'))
+				<a href="/admin" target="_blank">{{ trans("web.login_register.admin") }}</a>
+			@endif
+		@endif
+
+		<li class="dropdown">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+				aria-haspopup="true" aria-expanded="false">
+				 <img src="{{ $flagsLanguage[$lang] }}" alt="{{ $langName }}"
+                            style="width: 16px; height: 11px;" width="16" height="11">
+				{{ $langName }}
+				<span class="caret"></span>
+			</a>
+			<ul class="dropdown-menu">
+				@foreach (Config::get('app.locales') as $key => $value)
+					@if ($key != $lang)
+						<li>
+							<a href="{{ "/$key" . $urlToOtherLanguage }}">
+								<img src="{{ $flagsLanguage[$key] }}" alt="{{ $key }}"
+									style="width: 16px; height: 11px;" width="16" height="11">
+								{{ $value }}
+							</a>
+						</li>
+					@endif
+				@endforeach
+			</ul>
+		</li>
+    </div>
+
     <nav class="navbar navbar-default navbar-tauler">
         <div class="container-fluid">
             <div class="navbar-header">
@@ -38,24 +76,92 @@
                 </a>
             </div>
             <div class="navbar-collapse collapse" id="navbar" aria-expanded="false" style="height: 1px;">
+
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li class="dropdown">
+                    <li>
+                        <a href="{{ wpLink('wp_home') }}">
+                            {{ trans('web.home.home') }}
+                        </a>
+                    </li>
+                    <li class="dropdown active">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                            aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            aria-haspopup="true" aria-expanded="false">{{ trans('web.foot.auctions') }} <span
+                                class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider" role="separator"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
+                            <li><a href="{{ wpLink('wp_auctions') }}">
+                                    {{ trans('web.foot.auctions') }}
+                                </a>
+                            </li>
+                            <li><a href="#">Histórico</a></li>
+                            <li><a href="{{ wpLink('wp_events') }}">{{ trans('web.foot.events') }}</a></li>
+
                         </ul>
                     </li>
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-target="{{ wpLink('wp_services') }}" data-toggle="dropdown"
+                            href="{{ wpLink('wp_services') }}" role="button" aria-haspopup="true"
+                            aria-expanded="false">{{ trans('web.services.title') }} <span class="caret"></span></a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ wpLink('wp_sell_coins') }}">
+                                    {{ trans('web.foot.how_to_sell') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ wpLink('wp_buy_coins') }}">
+                                    {{ trans('web.foot.how_to_buy') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ wpLink('wp_valuations') }}">
+                                    {{ trans('web.foot.valuations') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ wpLink('wp_photography') }}">
+                                    {{ trans('web.foot.photography') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ wpLink('wp_coin_grading') }}">
+                                    {{ trans('web.foot.coin_grading') }}
+                                </a>
+                            </li>
+
+                        </ul>
+                    </li>
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" data-target="{{ wpLink('wp_about_us') }}"
+                            href="{{ wpLink('wp_about_us') }}" role="button" aria-haspopup="true"
+                            aria-expanded="false">{{ trans('web.foot.about_us') }} <span class="caret"></span></a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ wpLink('wp_faq') }}">
+                                    {{ trans('web.foot.faq') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ wpLink('wp_blog') }}">
+                                    {{ trans('web.foot.blog') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ wpLink('wp_term_condition') }}">
+                                    {{ trans('web.foot.auctions_conditions') }}
+                                </a>
+                            </li>
+
+                        </ul>
+                    </li>
+
+                    <li><a href="{{ wpLink('wp_contact') }}"
+                            title="{{ trans('web.foot.contact') }}">{{ trans('web.foot.contact') }}</a></li>
                 </ul>
+
             </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
     </nav>
