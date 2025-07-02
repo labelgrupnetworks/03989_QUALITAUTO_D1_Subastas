4cc1efc38dc9945751c97c12cda80deef3f7e0cf
Eloy N <enadal@labelgrup.com>
2025-07-02 09:14:15 +0200

    Tauler - Change in emails

--- resources/views/themes/tauleryfau/emails/new_emails/es/lotes-consignados.blade.php
+++ resources/views/themes/tauleryfau/emails/new_emails/es/lotes-consignados.blade.php
@@ -225,7 +225,7 @@
 <w:anchorlock/>
 <v:textbox inset="0px,0px,0px,0px">
 <center dir="false" style="color:#ffffff;font-family:Georgia, 'Times New Roman', serif;font-size:18px">
-<![endif]--><span class="button" style="background-color: #90825f; border-bottom: 0px solid transparent; border-left: 0px solid transparent; border-radius: 4px; border-right: 0px solid transparent; border-top: 0px solid transparent; color: #ffffff; display: inline-block; font-family: 'Bitter', Georgia, Times, 'Times New Roman', serif; font-size: 18px; font-weight: 700; mso-border-alt: none; padding-bottom: 5px; padding-top: 5px; padding-left: 30px; padding-right: 30px; text-align: center; width: auto; word-break: keep-all; letter-spacing: normal;"><span style="word-break: break-word; line-height: 36px;">VER LOTES CONSIGNADOS</span></span><!--[if mso]></center></v:textbox></v:roundrect><![endif]--></a></div>
+<![endif]--><span class="button" style="background-color: #90825f; border-bottom: 0px solid transparent; border-left: 0px solid transparent; border-radius: 4px; border-right: 0px solid transparent; border-top: 0px solid transparent; color: #ffffff; display: inline-block; font-family: 'Bitter', Georgia, Times, 'Times New Roman', serif; font-size: 18px; font-weight: 700; mso-border-alt: none; padding-bottom: 5px; padding-top: 5px; padding-left: 5px; padding-right: 5px; text-align: center; width: auto; word-break: keep-all; letter-spacing: normal;"><span style="word-break: break-word; line-height: 36px;">VER LOTES CONSIGNADOS</span></span><!--[if mso]></center></v:textbox></v:roundrect><![endif]--></a></div>
 </td>
 </tr>
 </table>
@@ -271,7 +271,7 @@
 <w:anchorlock/>
 <v:textbox inset="0px,0px,0px,0px">
 <center dir="false" style="color:#ffffff;font-family:Georgia, 'Times New Roman', serif;font-size:18px">
-<![endif]--><span class="button" style="background-color: #90825f; border-bottom: 0px solid transparent; border-left: 0px solid transparent; border-radius: 4px; border-right: 0px solid transparent; border-top: 0px solid transparent; color: #ffffff; display: inline-block; font-family: 'Bitter', Georgia, Times, 'Times New Roman', serif; font-size: 18px; font-weight: 700; mso-border-alt: none; padding-bottom: 5px; padding-top: 5px; padding-left: 30px; padding-right: 30px; text-align: center; width: auto; word-break: keep-all; letter-spacing: normal;"><span style="word-break: break-word; line-height: 36px;">CALENDARIO SUBASTAS</span></span><!--[if mso]></center></v:textbox></v:roundrect><![endif]--></a></div>
+<![endif]--><span class="button" style="background-color: #90825f; border-bottom: 0px solid transparent; border-left: 0px solid transparent; border-radius: 4px; border-right: 0px solid transparent; border-top: 0px solid transparent; color: #ffffff; display: inline-block; font-family: 'Bitter', Georgia, Times, 'Times New Roman', serif; font-size: 18px; font-weight: 700; mso-border-alt: none; padding-bottom: 5px; padding-top: 5px; padding-left: 5px; padding-right: 5px; text-align: center; width: auto; word-break: keep-all; letter-spacing: normal;"><span style="word-break: break-word; line-height: 36px;">CALENDARIO SUBASTAS</span></span><!--[if mso]></center></v:textbox></v:roundrect><![endif]--></a></div>
 </td>
 </tr>
 </table>
