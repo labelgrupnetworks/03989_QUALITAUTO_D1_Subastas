dfb7fbdfa8abcdfc5a7fe866c0f8dbf260a62e08
Eloy N <enadal@labelgrup.com>
2025-07-16 17:14:31 +0200

    Admin - Refactor resources

--- app/Http/Controllers/admin/ResourceController.php
+++ app/Http/Controllers/admin/ResourceController.php
@@ -3,101 +3,114 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Resources;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Config;
 
 class ResourceController extends Controller
 {
-
 	//Ver todos los Recursos que hay
-	public function index()
+	public function index(Request $request)
 	{
-
-		$content = new Resources();
-		if (!empty($_GET["see"]) && $_GET["see"] == 'A') {
-			$value_where = "and WEB_RESOURCE.type IN ('A')";
-		} elseif (!empty($_GET["see"]) && $_GET["see"] == 'C') {
-			$value_where = "and WEB_RESOURCE.type IN ('C')";
-		} else {
-			$value_where = "and WEB_RESOURCE.type IN ('I','H')";
-		}
-		if (!empty($_GET["crs"])) {
-			$value_where .= " and COD_BANNER_SEC = '" . $_GET["crs"] . "'";
+		$type = ['I', 'H'];
+		if ($request->input('see') == 'A') {
+			$type = ['A'];
+		} elseif ($request->input('see') == 'C') {
+			$type = ['C'];
 		}
 
-		$data['inf'] = $content->tableResource($value_where);
-		return View::make('admin::pages.resource', array('data' => $data));
+		$resouces = DB::table('web_resource')
+			->select(
+				DB::raw("(select id_web_resource from web_resource_banner where web_resource_banner.id_web_resource = web_resource.id_web_resource group by id_web_resource) as id_web_resource_banner"),
+				'id_web_resource',
+				'title',
+				'content'
+			)
+			->where('id_emp', config('app.main_emp'))
+			->whereIn('type', $type)
+			->when($request->input('crs'), function ($query) use ($request) {
+				return $query->where('cod_banner_sec', $request->input('crs'));
+			})
+			->orderBy('title', 'asc')
+			->get();
+
+		$data['inf'] = $resouces;
+
+		return View::make('admin::pages.resource', ['data' => $data]);
 	}
 
 	//Ver la informacion del bloque si no existe todo vacio
 	public function SeeResources($id = NULL)
 	{
-		$content = new Resources();
+		$resource = DB::table('web_resource')
+			->where('id_web_resource', $id)
+			->where('id_emp', config('app.main_emp'))
+			->first();
 
-		$bloque = $content->infResource($id);
-		if (!count($bloque) > 0) {
-			$bloque = null;
-		}
-		return View::make('admin::pages.editResource', array('bloque' => $bloque[0] ?? null));
+		return View::make('admin::pages.editResource', ['bloque' => $resource]);
 	}
 
-	public function EditResources()
+	public function EditResources(Request $request)
 	{
-		$content = new Resources();
-		$enabled_temp = 'off';
-		$new_windows_temp = 'off';
-		$name = Request::input('name');
-		$url_link = Request::input('url_link');
-		$new_windows_temp = Request::input('new_windows');
-		$type = Request::input('type');
-		$enabled_temp = Request::input('enabled');
-		$id = Request::input('id');
-		$file_url = Request::input('file_url');
-		$cache = Request::input('cache');
-		$cod_sec = Request::input('cod_sec');
+		$type = $request->input('type');
 
-		if ($type == 'H' || $type == 'A') {
-			$html = Request::input('html');
-		} elseif ($type == 'C') {
-			$html = Request::input('fecha');
-		} else {
-			$html = Request::input('text_html');
-		}
-		if (empty($cache)) {
-			$cache = 0;
-		}
+		$html = match ($type) {
+			'H' => $request->input('html'),
+			'A' => $request->input('html'),
+			'C' => $request->input('fecha'),
+			default => $request->input('text_html')
+		};
 
-		if ($enabled_temp == 'on') {
-			$enabled = 1;
-		} else {
-			$enabled = 0;
-		}
-		if ($new_windows_temp == 'on') {
-			$new_windows = 1;
-		} else {
-			$new_windows = 0;
-		}
+		$params = [
+			'title' => $request->input('name'),
+			'url_resource' => $request->input('file_url'),
+			'url_link' => $request->input('url_link', null),
+			'new_window' => $request->input('new_windows') == 'on' ? 1 : 0,
+			'type' => $type,
+			'content' => $html,
+			'enabled' => $request->input('enabled') == 'on' ? 1 : 0,
+			'time_cache' => $request->input('cache', 0),
+			'id_emp' => Config::get('app.main_emp'),
+			'creation_date' => date("Y-m-d H:i:s"),
+			'update_date' => date("Y-m-d H:i:s")
+		];
 
-		if (empty($url_link)) {
-			$url_link = null;
-		}
+		$id = $request->input('id', 0);
 
 		if ($id < 1) {
-			$id = $content->newResource($name, $url_link, $new_windows, $type, $enabled, $html, $file_url, $cache, $cod_sec);
-			return $id;
+			// Crear nuevo recurso
+			$max_id = DB::table('web_resource')->max('id_web_resource');
+			$max_id = $max_id + 1;
+			$params['id_web_resource'] = $max_id;
+
+			DB::table('web_resource')->insert($params);
+			$id = $max_id;
 		} else {
-			//no se modificara la seccion por lo que no se evnia
-			$content->updateResource($name, $url_link, $new_windows, $type, $enabled, $id, $html, $file_url, $cache);
-			return $id;
+			// Actualizar recurso existente
+			$params['cod_banner_sec'] = $request->input('cod_sec', null);
+
+			DB::table('web_resource')
+				->where('id_web_resource', $id)
+				->where('id_emp', Config::get('app.main_emp'))
+				->update($params);
 		}
+
+		return $id;
 	}
 
-	public function DeleteResource()
+	public function DeleteResource(Request $request)
 	{
-		$content = new Resources();
-		$id_delete = Request::input('id_resource');
-		$content->delete_Resource($id_delete);
-		$content->delete_ResourceBanner($id_delete);
+		$id_delete = $request->input('id_resource');
+		DB::table('web_resource')
+			->where('id_web_resource', $id_delete)
+			->where('id_emp', Config::get('app.main_emp'))
+			->delete();
+
+		DB::table('web_resource_banner')
+			->where('id_web_resource', $id_delete)
+			->delete();
+
+		return response()->json(['status' => 'success', 'message' => 'Resource deleted successfully']);
 	}
 }
