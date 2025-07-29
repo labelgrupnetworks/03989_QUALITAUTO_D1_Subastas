66e3f2c85c1f0138575036e71ad540e7bb465309
Eloy N <enadal@labelgrup.com>
2025-07-29 16:27:58 +0200

    Soler - Change design and styles to ficha cerrada

--- resources/views/themes/soleryllach/includes/ficha/pujas_ficha_cerrada.blade.php
+++ resources/views/themes/soleryllach/includes/ficha/pujas_ficha_cerrada.blade.php
@@ -1,5 +1,7 @@
 @php
-    $precio_venta = null;
+	use App\Support\Date;
+
+	$precio_venta = null;
     if (!empty($lote_actual->himp_csub)) {
         $precio_venta = $lote_actual->himp_csub;
     }
@@ -16,33 +18,49 @@
     //Si hay precio de venta y impsalweb_asigl0 impsalweb_asigl0 contiene valor, mostramos este como precio de venta
     $precio_venta =
         !empty($precio_venta) && $lote_actual->impsalweb_asigl0 != 0 ? $lote_actual->impsalweb_asigl0 : $precio_venta;
-@endphp
-<div class="info_single col-xs-12">
-    <div class="info_single_title col-xs-12">
-        {{ trans(\Config::get('app.theme') . '-app.subastas.finalized') }}
-    </div>
-    <div class="row">
-        <div class="col-xs-12 col-sm-6">
 
-            @if (
-                ($lote_actual->cerrado_asigl0 == 'S' && $lote_actual->remate_asigl0 == 'S' && !empty($precio_venta)) ||
-                    ($lote_actual->subc_sub == 'H' && !empty($lote_actual->impadj_asigl0)))
-                <p class="pre">{{ trans(\Config::get('app.theme') . '-app.subastas.buy_to') }}:</p>
-                <div class="pre">
-                    {{ \Tools::moneyFormat($lote_actual->subc_sub == 'H' && !empty($lote_actual->impadj_asigl0) ? $lote_actual->impadj_asigl0 : $precio_venta) }}
-                    {{ trans(\Config::get('app.theme') . '-app.subastas.euros') }}</div>
-            @elseif($lote_actual->cerrado_asigl0 == 'S' && (!empty($precio_venta) || $lote_actual->desadju_asigl0 == 'S'))
-                <p class="pre"> {{ trans(\Config::get('app.theme') . '-app.subastas.buy') }}</p>
-            @elseif(strtoupper($lote_actual->tipo_sub) == 'V' &&
-                    $lote_actual->cerrado_asigl0 == 'N' &&
-                    $lote_actual->end_session > date('now'))
-                <p class="pre">{{ trans(\Config::get('app.theme') . '-app.subastas.dont_buy') }}</p>
-            @elseif($lote_actual->cerrado_asigl0 == 'S' && empty($precio_venta))
-                <p class="pre">{{ trans(\Config::get('app.theme') . '-app.subastas.dont_buy') }}</p>
-            @elseif($lote_actual->cerrado_asigl0 == 'D')
-                <p class="pre">{{ trans(\Config::get('app.theme') . '-app.subastas.dont_available') }}</p>
-            @endif
-            <p class="pre"></p>
+	$endSession = Date::toISOFormat($lote_actual->end_session, 'DD MMMM YYYY | HH\h mm');
+@endphp
+<div class="ficha_C">
+    <div class="ficha_prices">
+        <p class="price_label">{{ trans('web.subastas.price_sale') }}</p>
+        <p class="price_value">
+            {{ $lote_actual->formatted_impsalhces_asigl0 }} {{ trans('web.subastas.euros') }}
+        </p>
+    </div>
+
+    @if (
+        ($lote_actual->cerrado_asigl0 == 'S' && $lote_actual->remate_asigl0 == 'S' && !empty($precio_venta)) ||
+            ($lote_actual->subc_sub == 'H' && !empty($lote_actual->impadj_asigl0)))
+        <div class="ficha_prices">
+            <p class="price_label">{{ Str::title(trans('web.subastas.buy_to')) }}</p>
+            <p class="price_value">
+                {{ Tools::moneyFormat($lote_actual->subc_sub == 'H' && !empty($lote_actual->impadj_asigl0) ? $lote_actual->impadj_asigl0 : $precio_venta) }}
+                {{ trans('web.subastas.euros') }}
+            </p>
         </div>
+    @elseif($lote_actual->cerrado_asigl0 == 'S' && (!empty($precio_venta) || $lote_actual->desadju_asigl0 == 'S'))
+        <div class="ficha_prices">
+            <p class="price_label">{{ trans('web.subastas.buy') }}</p>
+        </div>
+    @elseif(strtoupper($lote_actual->tipo_sub) == 'V' &&
+            $lote_actual->cerrado_asigl0 == 'N' &&
+            $lote_actual->end_session > date('now'))
+        <div class="ficha_prices">
+            <p class="price_label">{{ trans('web.subastas.dont_buy') }}</p>
+        </div>
+    @elseif($lote_actual->cerrado_asigl0 == 'S' && empty($precio_venta))
+        <div class="ficha_prices">
+            <p class="price_label">{{ trans('web.subastas.dont_buy') }}</p>
+        </div>
+    @elseif($lote_actual->cerrado_asigl0 == 'D')
+        <div class="ficha_prices">
+            <p class="price_label">{{ trans('web.subastas.dont_available') }}</p>
+        </div>
+    @endif
+
+    <div class="ficha_shares">
+        <p>{{ $endSession }}</p>
+        @include('includes.ficha.share')
     </div>
 </div>
