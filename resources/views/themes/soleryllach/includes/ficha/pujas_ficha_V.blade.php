82a6a7eac0f13616e164b7b794639c75df41bf1d
Eloy N <enadal@labelgrup.com>
2025-07-29 12:44:15 +0200

    Soler - Change design and styles ficha V

--- resources/views/themes/soleryllach/includes/ficha/pujas_ficha_V.blade.php
+++ resources/views/themes/soleryllach/includes/ficha/pujas_ficha_V.blade.php
@@ -1,56 +1,33 @@
-<div class="info_single ficha_V col-xs-12">
-    <div class="info_single_title col-xs-12">
-        <div class="sub-o">
+@php
+	use App\Support\Date;
 
-           <?php // entraran tanto lotes de subastas V como cerrados de otra con posibilidad de compra  ?>
+    $showBuyButton =
+        $lote_actual->retirado_asigl0 == 'N' &&
+        empty($lote_actual->himp_csub) &&
+        ($lote_actual->subc_sub == 'S' || $lote_actual->subc_sub == 'A');
 
-            <p>
-                @if ($lote_actual->tipo_sub == 'V')
-                    {{ trans(\Config::get('app.theme').'-app.subastas.lot_subasta_venta') }}
-                @elseif($lote_actual->tipo_sub == 'W')
-                    {{ trans(\Config::get('app.theme').'-app.subastas.lot_subasta_presencial') }}
-                @elseif($lote_actual->tipo_sub == 'O')
-                    {{ trans(\Config::get('app.theme').'-app.subastas.lot_subasta_online') }}
-                @endif
-            </p>
+	$endSession = Date::toISOFormat($lote_actual->end_session, 'DD MMMM YYYY | HH\h mm');
+@endphp
 
-            @if($lote_actual->cerrado_asigl0 == 'N')
-                <span class="clock"><i class="fa fa-clock-o"></i><span data-countdown="{{ strtotime($lote_actual->end_session) - getdate()[0] }}"  data-format="<?= \Tools::down_timer($lote_actual->end_session); ?>" data-closed="{{ $lote_actual->cerrado_asigl0 }}" class="timer"></span>
-            @endif
-        </div>
-        <div class="date_top_side_small">
-
-            @if($lote_actual->cerrado_asigl0 == 'N')
-                <span class="cierre_lote"></span>
-            @endif
-          <?php /* no ponemos CET   <span id="cet_o"> {{ trans(\Config::get('app.theme').'-app.lot.cet') }}</span> */ ?>
-        </div>
+<div class="ficha_V">
+    <div class="ficha_prices">
+        <p class="price_label">{{ trans('web.subastas.price_sale') }}</p>
+        <p class="price_value">{{ $lote_actual->formatted_actual_bid }} {{ trans('web.subastas.euros') }}</p>
     </div>
 
-    <div class="col-xs-12 col-sm-6">
-        <p class="pre">{{ trans(\Config::get('app.theme').'-app.subastas.price_sale') }}</p>
-        <div class="pre">{{$lote_actual->formatted_actual_bid}} {{ trans(\Config::get('app.theme').'-app.subastas.euros') }}</div>
-    </div>
-    <div class="col-xs-12">
-        <div class="info_single_content info_single_button">
-            @if ($lote_actual->retirado_asigl0 == 'N' && empty($lote_actual->himp_csub) && ($lote_actual->subc_sub == 'S' || $lote_actual->subc_sub == 'A'))
-            <br>   <button data-from="modal" class="lot-action_comprar_lot btn btn-lg btn-custom btn-color" type="button" ref="{{ $data['subasta_info']->lote_actual->ref_asigl0 }}" codsub="{{ $data['subasta_info']->lote_actual->cod_sub }}"><i class="fa fa-shopping-cart" aria-hidden="true"></i> {{ trans(\Config::get('app.theme').'-app.subastas.buy_lot') }}</button>
-           <br> <br>
-           @endif
+    @if ($showBuyButton)
+        <div class="ficha_actions">
+            <button class="lot-action_comprar_lot btn btn-bold btn-color" data-from="modal" type="button"
+                ref="{{ $data['subasta_info']->lote_actual->ref_asigl0 }}"
+                codsub="{{ $data['subasta_info']->lote_actual->cod_sub }}">
+                {{ trans('web.subastas.buy_lot') }}
+            </button>
         </div>
+    @endif
+
+    <div class="ficha_shares">
+        <p>{{ $endSession }}</p>
+        @include('includes.ficha.share')
     </div>
 
 </div>
-
-<script>
-   $(document).ready(function() {
-        //calculamos la fecha de cierre
-     //   $("#cierre_lote").html(format_date_large(new Date("{{$lote_actual->end_session}}".replace(/-/g, "/"))),"");
-        $(".cierre_lote").html(format_date_large(new Date("{{$lote_actual->end_session}}".replace(/-/g, "/")),''));
-
-    });
-</script>
-
-
-
-
