1738364ea8da38af01f75b701db1638640afcebb
Eloy N <enadal@labelgrup.com>
2025-08-13 13:43:28 +0200

    Core - Add command to update categoires in web_config

---
+++ app/Console/Commands/Scripts/UpdateCategoryInWebConfig.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands\Scripts;
+
+use App\Models\V5\Web_Config;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+
+
+class UpdateCategoryInWebConfig extends Command
+{
+	protected $signature = 'label:update-webconfig-categories';
+	protected $description = 'Actualizar las categorías en la tabla web_config según los archivos de config/web';
+
+	public function handle()
+	{
+		$categories = Web_Config::getSections();
+
+		$dataBaseConfigs = Web_Config::query()
+			->withoutGlobalScopes()
+			->whereNull('category')
+			->get();
+
+		foreach ($categories as $category) {
+			$categoriesFile = Config::get('app.' . $category, []);
+			$inCategories = $dataBaseConfigs->whereIn('key', array_keys($categoriesFile));
+
+			$inCategories->each(function ($config) use ($category) {
+				// Actualizar la categoría en la base de datos
+				$config->category = $category;
+				$config->updated_by = 'script';
+				$config->save();
+
+				$this->info("Configuración actualizada: {$config->key}");
+			});
+		}
+
+		$this->info('Categorías actualizadas correctamente en web_config.');
+		return 0;
+	}
+}
