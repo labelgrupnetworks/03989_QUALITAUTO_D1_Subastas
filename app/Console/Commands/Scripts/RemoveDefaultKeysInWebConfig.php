f072e5db86b2cac5dc21a2ffc4ff560053de4e60
Eloy N <enadal@labelgrup.com>
2025-08-21 15:16:58 +0200

    Core - Add command to remove default configuration keys from Web_Config

---
+++ app/Console/Commands/Scripts/RemoveDefaultKeysInWebConfig.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands\Scripts;
+
+use App\Models\V5\Web_Config;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+
+
+class RemoveDefaultKeysInWebConfig extends Command
+{
+	protected $signature = 'label:remove-default-webconfig-keys';
+	protected $description = 'Eliminar claves que tienen el valor por defecto';
+
+	public function handle()
+	{
+		$defaultValues = Config::get('label.app', []);
+		$dbValues = Web_Config::query()
+		 	->pluck('value', 'key')
+			->toArray();
+
+
+		$sameValues = [];
+		foreach ($dbValues as $key => $value) {
+			if (isset($defaultValues[$key]) && ($defaultValues[$key] == $value) || (empty($defaultValues[$key]) && empty($value))) {
+				$sameValues[$key] = $value;
+			}
+		}
+
+		$number = Web_Config::query()
+			->whereIn('key', array_keys($sameValues))
+			->count();
+
+		$result = $this->ask("Se eliminarán $number claves de configuración que tienen el valor por defecto. ¿Está seguro de continuar? (s/n)");
+
+		if (strtolower($result) !== 's') {
+			$this->info('Operación cancelada.');
+			return 0;
+		}
+
+		$number = Web_Config::query()
+			->whereIn('key', array_keys($sameValues))
+			->delete();
+
+		$this->info("Se eliminaron $number claves de configuración.");
+		return 0;
+	}
+}
