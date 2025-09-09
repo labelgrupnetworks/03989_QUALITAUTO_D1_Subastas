cabcd4b7305a88fdf7707979a4e1cc50095147fd
Eloy N <enadal@labelgrup.com>
2025-09-09 17:32:40 +0200

    Test - Change remove valoracion file

--- app/Http/Controllers/ValoracionController.php
+++ app/Http/Controllers/ValoracionController.php
@@ -159,7 +159,7 @@ class ValoracionController extends Controller
 
 			$isSended = ToolsServiceProvider::sendMail('notification_valoracion', $emailOptions);
 			if(!$isSended) {
-				$this->removeFiles(Input::file('imagen', []));
+				//$this->removeFiles(Input::file('imagen', []));
 				return [
 					'status'  => 'error',
 				];
@@ -175,7 +175,7 @@ class ValoracionController extends Controller
 				ToolsServiceProvider::sendMail('notification_valoracion', $emailOptions);
 			}
 
-			$this->removeFiles(Input::file('imagen', []));
+			//$this->removeFiles(Input::file('imagen', []));
 
 			return [
 				'status'  => 'correct',
