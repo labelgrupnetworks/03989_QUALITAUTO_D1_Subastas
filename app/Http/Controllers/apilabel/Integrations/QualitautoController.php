973c76713bdbe0c2309edff8eee672393a48d614
Eloy N <enadal@labelgrup.com>
2025-09-15 18:28:12 +0200

    Qualitauto - Add lot import

--- app/Http/Controllers/apilabel/Integrations/QualitautoController.php
+++ app/Http/Controllers/apilabel/Integrations/QualitautoController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\apilabel\Integrations;
 
+use App\Http\Controllers\apilabel\LotController;
+use App\Models\V5\FgAsigl0;
+use App\Models\V5\FgCaracteristicas;
+use App\Models\V5\FgCaracteristicas_Value;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 
 class QualitautoController
 {
@@ -15,8 +20,17 @@ class QualitautoController
 
 		$vehicle = data_get($array, 'Dossier.Vehicle', []);
 		if (empty($vehicle)) {
-			return response()->json(['message' => 'No vehicle data found'], 400);
+			//docs/HttpFiles/example_data.txt
+			$file = file(base_path('docs/HttpFiles/example_data.txt'));
+			$xml_data = implode("", $file);
+			$xml = simplexml_load_string($xml_data);
+			$json = json_encode($xml);
+			$array = json_decode($json, true);
+			$vehicle = data_get($array, 'Dossier.Vehicle', []);
+
+			//return response()->json(['message' => 'No vehicle data found'], 400);
 		}
+
 
 		$colores = data_get($vehicle, 'LibroVin.Colores.color', []);
 		$fotos = data_get($vehicle, 'FotosCargadas.Foto', []);
@@ -33,8 +47,95 @@ class QualitautoController
 			'bastidor' => data_get($vehicle, 'VehicleIdentNumber', null), //usar como id.
 			'matricula' => data_get($vehicle, 'RegistrationData.LicenseNumber', null),
 			'fecha_matriculacion' => data_get($vehicle, 'InitialRegistration', null),
+			'tarjeta_emision' => data_get($vehicle, 'VehicleData.EmissionClass', null),
+			'colores' => is_array($colores) ? $colores : [$colores],
+			'fotos' => is_array($fotos) ? $fotos : [$fotos],
 		];
 
-		return response()->json($vehicleData);
+		$auctionId = 'LABELO';
+		$existingLots = FgAsigl0::query()
+			->joinFghces1Asigl0()
+			->where('sub_asigl0', $auctionId)
+			->pluck('idorigen_hces1', 'ref_asigl0');
+
+		$lotId = "{$auctionId}-{$vehicleData['bastidor']}";
+
+		//create lot object
+		$lotObject = $this->createLotObject($lotId, $vehicleData);
+
+		$lotControler = new LotController();
+		$json = $lotControler->createLot([$lotObject]);
+		$result = json_decode($json);
+
+
+
+
+		// if ($existingLots->contains($lotId)) {
+		// 	//update
+		// }
+
+		//create
+
+		//action code -> LABELO
+
+		return response()->json($result);
+	}
+
+	private function createLotObject($id, $vehicleData)
+	{
+		$idAuction = 'LABELO';
+		$maxReference = FgAsigl0::where('sub_asigl0', $idAuction)->max('ref_asigl0') + 1;
+
+		$lot = [
+			'idorigin' => $id,
+			'title' => "{$vehicleData['fabricante']} {$vehicleData['modelo']}",
+			'description' => "{$vehicleData['fabricante']} {$vehicleData['modelo']}",
+			'search' => "{$vehicleData['fabricante']} {$vehicleData['modelo']}",
+			'idsubcategory' => Config::get('app.default_idsubcategory', 'VM'),
+			'idauction' => $idAuction,
+			'reflot' => $maxReference,
+			'features' => $this->addFeatures($vehicleData),
+			'startprice' => 0,
+			'hidden' => 'S',
+		];
+
+		return $lot;
+	}
+
+	private function addFeatures($vehicleData)
+	{
+		$fgCaracteristicas = FgCaracteristicas::query()
+			->select('name_caracteristicas', 'id_caracteristicas', 'filtro_caracteristicas', 'value_caracteristicas')
+			->get();
+
+		$features = [];
+
+		foreach ($vehicleData as $featureName => $featureValue) {
+
+			//filtramos las caracteristicas que coinciden con las creadas en la base de datos
+			$caracteristica = $fgCaracteristicas->filter(function ($item) use ($featureName) {
+				return mb_strtolower($item->name_caracteristicas) == mb_strtolower($featureName);
+			})->first();
+
+			if ($caracteristica) {
+
+				if ($caracteristica->value_caracteristicas == 'N') {
+					$idvaluefeature = null;
+					$newValue = $featureValue;
+				} else {
+					$newFeature = FgCaracteristicas_Value::addFeature($caracteristica->id_caracteristicas, $featureValue);
+					$idvaluefeature = $newFeature['idFeatureValue'];
+					$newValue = null;
+				}
+
+				$feature['idfeature'] = $caracteristica->id_caracteristicas;
+				$feature['idvaluefeature'] = $idvaluefeature;
+				$feature['value'] = $newValue;
+
+				$features[$caracteristica->name_caracteristicas] = $feature;
+			}
+		}
+
+		return $features;
 	}
 }
